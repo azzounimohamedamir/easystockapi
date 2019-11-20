8d57efa9a0229af07f06297953ff2e57ac62f767
Khaireddine MANSOURI <k_mansouri@esi.dz>
2019-11-21 00:00:19 +0100

    Implement the new design for the chair count Popup  - Part1

--- SmartRestaurant.Forms/SRMobile/Views/Popups/ChairSelectionPopup.xaml.cs
+++ SmartRestaurant.Forms/SRMobile/Views/Popups/ChairSelectionPopup.xaml.cs
@@ -27,6 +27,8 @@ namespace SmartRestaurant.Diner.Views.Popups
 
             BindingContext = _model;
             oldvalue = _model.SelectedTable.NombreChaises;
+            minus_enabled = _model.SelectedTable.NombreChaises>0;
+            plus_enabled = _model.SelectedTable.NombreChaises<99;
             viewmodel = _model;
 
 
@@ -52,8 +54,36 @@ namespace SmartRestaurant.Diner.Views.Popups
                 {
                     ((ZonesListViewModel)BindingContext).SelectedTable.NombreChaises = ((ZonesListViewModel)BindingContext).SelectedTable.NombreChaises;
                 }
-                    }
+                if ((Convert.ToInt32(e.NewTextValue)) >99)
+                {
+                    
+                    ((ZonesListViewModel)BindingContext).SelectedTable.NombreChaises = 99;
+                }
+            }
             catch { ((ZonesListViewModel)BindingContext).SelectedTable.NombreChaises = 0; }
+        }
+        bool minus_enabled;
+        bool plus_enabled;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (((ZonesListViewModel)BindingContext).SelectedTable.NombreChaises <99)
+                ((ZonesListViewModel)BindingContext).SelectedTable.NombreChaises++;
+            else
+            
+                plus_enabled = false;
+            minus_enabled = true;
+            
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            if (((ZonesListViewModel)BindingContext).SelectedTable.NombreChaises > 0)
+                ((ZonesListViewModel)BindingContext).SelectedTable.NombreChaises--;
+            else
+           
+                minus_enabled = false;
+            plus_enabled = true;
+            
         }
     }
 }
