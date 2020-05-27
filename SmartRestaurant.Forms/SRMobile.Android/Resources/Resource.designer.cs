2958775225d091235be7ea60cd37112f421209a3
Khaoula Bouzekria <khaoulabouzekria@outlook.com>
2020-05-27 20:14:28 +0100

    Adding Chicken dishes to listSections

--- SmartRestaurant.Forms/SRMobile.Android/Resources/Resource.designer.cs
+++ SmartRestaurant.Forms/SRMobile.Android/Resources/Resource.designer.cs
@@ -14530,124 +14530,151 @@ namespace SmartRestaurant.Diner.Droid
 			public const int Pomme_de_terre = 2131165369;
 			
 			// aapt resource value: 0x7F0700BA
-			public const int Prev = 2131165370;
+			public const int Poulet_farci = 2131165370;
 			
 			// aapt resource value: 0x7F0700BB
-			public const int salade_cezar = 2131165371;
+			public const int Poulet_farci2 = 2131165371;
 			
 			// aapt resource value: 0x7F0700BC
-			public const int salade_cezar2 = 2131165372;
+			public const int Poulet_farci3 = 2131165372;
 			
 			// aapt resource value: 0x7F0700BD
-			public const int salade_cezar3 = 2131165373;
+			public const int Poulet_pane = 2131165373;
 			
 			// aapt resource value: 0x7F0700BE
-			public const int salade_de_thon = 2131165374;
+			public const int Poulet_pane2 = 2131165374;
 			
 			// aapt resource value: 0x7F0700BF
-			public const int salade_de_thon2 = 2131165375;
+			public const int Poulet_pane3 = 2131165375;
 			
 			// aapt resource value: 0x7F0700C0
-			public const int salade_de_thon3 = 2131165376;
+			public const int Poulet_roti = 2131165376;
 			
 			// aapt resource value: 0x7F0700C1
-			public const int salads = 2131165377;
+			public const int Poulet_roti2 = 2131165377;
 			
 			// aapt resource value: 0x7F0700C2
-			public const int sandwich = 2131165378;
+			public const int Poulet_roti3 = 2131165378;
 			
 			// aapt resource value: 0x7F0700C3
-			public const int soda = 2131165379;
+			public const int Prev = 2131165379;
 			
 			// aapt resource value: 0x7F0700C4
-			public const int Soupe_de_legumes = 2131165380;
+			public const int salade_cezar = 2131165380;
 			
 			// aapt resource value: 0x7F0700C5
-			public const int Soupe_de_legumes2 = 2131165381;
+			public const int salade_cezar2 = 2131165381;
 			
 			// aapt resource value: 0x7F0700C6
-			public const int Soupe_de_legumes3 = 2131165382;
+			public const int salade_cezar3 = 2131165382;
 			
 			// aapt resource value: 0x7F0700C7
-			public const int soupe_de_lentilles = 2131165383;
+			public const int salade_de_thon = 2131165383;
 			
 			// aapt resource value: 0x7F0700C8
-			public const int soupe_de_lentilles2 = 2131165384;
+			public const int salade_de_thon2 = 2131165384;
 			
 			// aapt resource value: 0x7F0700C9
-			public const int soupe_de_lentilles3 = 2131165385;
+			public const int salade_de_thon3 = 2131165385;
 			
 			// aapt resource value: 0x7F0700CA
-			public const int Soupe_de_poisson = 2131165386;
+			public const int salads = 2131165386;
 			
 			// aapt resource value: 0x7F0700CB
-			public const int soupe_de_poisson2 = 2131165387;
+			public const int sandwich = 2131165387;
 			
 			// aapt resource value: 0x7F0700CC
-			public const int soupe_de_poisson3 = 2131165388;
+			public const int soda = 2131165388;
 			
 			// aapt resource value: 0x7F0700CD
-			public const int Spaghetti = 2131165389;
+			public const int Soupe_de_legumes = 2131165389;
 			
 			// aapt resource value: 0x7F0700CE
-			public const int Spaghetti2 = 2131165390;
+			public const int Soupe_de_legumes2 = 2131165390;
 			
 			// aapt resource value: 0x7F0700CF
-			public const int Spaghetti3 = 2131165391;
+			public const int Soupe_de_legumes3 = 2131165391;
 			
 			// aapt resource value: 0x7F0700D0
-			public const int Splash = 2131165392;
+			public const int soupe_de_lentilles = 2131165392;
 			
 			// aapt resource value: 0x7F0700D1
-			public const int splash_background = 2131165393;
+			public const int soupe_de_lentilles2 = 2131165393;
 			
 			// aapt resource value: 0x7F0700D2
-			public const int star = 2131165394;
+			public const int soupe_de_lentilles3 = 2131165394;
 			
 			// aapt resource value: 0x7F0700D3
-			public const int starters = 2131165395;
+			public const int Soupe_de_poisson = 2131165395;
 			
 			// aapt resource value: 0x7F0700D4
-			public const int supplement = 2131165396;
+			public const int soupe_de_poisson2 = 2131165396;
 			
 			// aapt resource value: 0x7F0700D5
-			public const int Table = 2131165397;
+			public const int soupe_de_poisson3 = 2131165397;
 			
 			// aapt resource value: 0x7F0700D6
-			public const int tacos = 2131165398;
+			public const int Spaghetti = 2131165398;
 			
 			// aapt resource value: 0x7F0700D7
-			public const int Tacos2 = 2131165399;
+			public const int Spaghetti2 = 2131165399;
 			
 			// aapt resource value: 0x7F0700D8
-			public const int Tacos3 = 2131165400;
+			public const int Spaghetti3 = 2131165400;
 			
 			// aapt resource value: 0x7F0700D9
-			public const int tartine_saumon = 2131165401;
+			public const int Splash = 2131165401;
 			
 			// aapt resource value: 0x7F0700DA
-			public const int tartine_saumon2 = 2131165402;
+			public const int splash_background = 2131165402;
 			
 			// aapt resource value: 0x7F0700DB
-			public const int tartine_saumon3 = 2131165403;
+			public const int star = 2131165403;
 			
 			// aapt resource value: 0x7F0700DC
-			public const int tomate = 2131165404;
+			public const int starters = 2131165404;
 			
 			// aapt resource value: 0x7F0700DD
-			public const int tooltip_frame_dark = 2131165405;
+			public const int supplement = 2131165405;
 			
 			// aapt resource value: 0x7F0700DE
-			public const int tooltip_frame_light = 2131165406;
+			public const int Table = 2131165406;
 			
 			// aapt resource value: 0x7F0700DF
-			public const int Up = 2131165407;
+			public const int tacos = 2131165407;
 			
 			// aapt resource value: 0x7F0700E0
-			public const int usa = 2131165408;
+			public const int Tacos2 = 2131165408;
 			
 			// aapt resource value: 0x7F0700E1
-			public const int Welcome = 2131165409;
+			public const int Tacos3 = 2131165409;
+			
+			// aapt resource value: 0x7F0700E2
+			public const int tartine_saumon = 2131165410;
+			
+			// aapt resource value: 0x7F0700E3
+			public const int tartine_saumon2 = 2131165411;
+			
+			// aapt resource value: 0x7F0700E4
+			public const int tartine_saumon3 = 2131165412;
+			
+			// aapt resource value: 0x7F0700E5
+			public const int tomate = 2131165413;
+			
+			// aapt resource value: 0x7F0700E6
+			public const int tooltip_frame_dark = 2131165414;
+			
+			// aapt resource value: 0x7F0700E7
+			public const int tooltip_frame_light = 2131165415;
+			
+			// aapt resource value: 0x7F0700E8
+			public const int Up = 2131165416;
+			
+			// aapt resource value: 0x7F0700E9
+			public const int usa = 2131165417;
+			
+			// aapt resource value: 0x7F0700EA
+			public const int Welcome = 2131165418;
 			
 			static Drawable()
 			{
