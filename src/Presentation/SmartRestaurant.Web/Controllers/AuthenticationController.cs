97e98594c3d43505e47be14c08aab067d5006f81
Amine Smahi <mohammed.amine.smahi@gmail.com>
2020-06-16 16:48:03 +0100

    Add TokenGenerator as a helper class

--- src/Presentation/SmartRestaurant.Web/Controllers/AuthenticationController.cs
+++ src/Presentation/SmartRestaurant.Web/Controllers/AuthenticationController.cs
@@ -2,15 +2,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using SmartRestaurant.Application.Users.Commands;
 using SmartRestaurant.Domain.Exceptions;
 using SmartRestaurant.Web.Helpers;
 using SmartRestaurant.Web.Models;
-using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SmartRestaurant.Web.Controllers
@@ -36,19 +31,7 @@ namespace SmartRestaurant.Web.Controllers
             if (user == null)
                 return BadRequest(new { message = "Username or password is incorrect" });
 
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, user.UserId.ToString())
-                }),
-                Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
+            string tokenString = TokenGenerator.Generate(user, _appSettings.Secret);
 
             UserModel userModel = _mapper.Map<UserModel>(user);
             userModel.Token = tokenString;
---
+++ src/Presentation/SmartRestaurant.Web/Helpers/TokenGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using SmartRestaurant.Application.Common.Dtos;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SmartRestaurant.Web.Helpers
+{
+    public static class TokenGenerator
+    {
+        public static string Generate(UserDto user,string secret)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserId.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return tokenString;
+        }
+    }
+}
