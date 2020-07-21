c7f645ea74b0a9909a39a0ad3e1cccb3af43d167
Amine Smahi <mohammed.amine.smahi@gmail.com>
2020-07-21 18:21:36 +0100

    Add swagger support

--- src/Presentation/SmartRestaurant.API/Startup.cs
+++ src/Presentation/SmartRestaurant.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using SmartRestaurant.Application;
 using SmartRestaurant.Infrastructure;
 using SmartRestaurant.Infrastructure.Identity;
@@ -26,7 +27,10 @@ namespace SmartRestaurant.API
             services.AddIdentityInfrastructure(Configuration);
 
             services.AddHttpContextAccessor();
-
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
             services.AddControllersWithViews();
         }
 
@@ -43,7 +47,11 @@ namespace SmartRestaurant.API
             }
 
             app.UseStaticFiles();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
