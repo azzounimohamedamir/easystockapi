1a04c2356c3507400fc30b6ea62fb350f90e56c7
Amine Smahi <mohammed.amine.smahi@gmail.com>
2020-07-23 10:29:16 +0100

    Allow CORS for devlopment

--- src/Presentation/SmartRestaurant.API/Startup.cs
+++ src/Presentation/SmartRestaurant.API/Startup.cs
@@ -27,6 +27,18 @@ namespace SmartRestaurant.API
             services.AddIdentityInfrastructure(Configuration);
 
             services.AddHttpContextAccessor();
+
+            // In developement : Allow frontend devs to test the api from their local env
+            // In production : Replace AllowAnyOrigin() with WithOrigins("http://smartrestaurant.io","https://smartrestaurant.io")
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+                });
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Smart Restaurant api v1", Version = "v1" });
@@ -52,6 +64,7 @@ namespace SmartRestaurant.API
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Smart Restaurant api v1");
             });
+            app.UseCors("AllowAll");
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
