56658ab2a41c519ef3957557988563227869174d
Amine Smahi <mohammed.amine.smahi@gmail.com>
2020-07-25 09:53:40 +0100

    Change file name from enums to roles

--- src/Presentation/SmartRestaurant.API/Startup.cs
+++ src/Presentation/SmartRestaurant.API/Startup.cs
@@ -58,7 +58,7 @@ namespace SmartRestaurant.API
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Smart Restaurant api v1");
             });
-            
+
             app.UseRouting();
 
             CORSConfiguration.UseCORS(app, env);
