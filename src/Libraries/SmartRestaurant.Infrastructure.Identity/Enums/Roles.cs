e073b5dc783daaeb626a64e381b5198e6c05abd6
Amine Smahi <mohammed.amine.smahi@gmail.com>
2020-08-03 00:08:18 +0100

    Add Smart Restaurant supported roles

--- src/Libraries/SmartRestaurant.Infrastructure.Identity/Enums/Roles.cs
+++ src/Libraries/SmartRestaurant.Infrastructure.Identity/Enums/Roles.cs
@@ -3,7 +3,16 @@
     public enum Roles
     {
         SuperAdmin = 1,
-        Admin = 2,
-        User = 3
+        SupportAgent = 2,
+        SalesMan = 3,
+        Photograph = 4,
+        RestaurantAdmin = 5,
+        RestaurantManager = 6,
+        Chef = 7,
+        Cashier = 8,
+        Waiter = 9,
+        Diner = 10,
+        Anounymous = 11,
+        Organization = 12
     }
 }
