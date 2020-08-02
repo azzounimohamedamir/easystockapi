6f4ef8369ed034d1f4e9f2bf85bde86459d1aa05
baouche iqbal <iqbal.baouche@univ-dbkm.dz>
2020-08-02 19:49:11 +0100

    [Fix] file helper

--- src/Presentation/SmartRestaurant.API/Helpers/FileHelper.cs
+++ src/Presentation/SmartRestaurant.API/Helpers/FileHelper.cs
@@ -12,23 +12,19 @@ namespace SmartRestaurant.API.Helpers
             foreach (var file in fileUploadApi.Files)
             {
                 var  imageModel = new ImageModel();
-                imageModel.ImageBytes = FileToByteArray(file.FileName);
-                imageModel.ImageTitle = file.FileName;
-                imageModel.IsLogo = fileUploadApi.IsLogo;
-                yield return imageModel;
+                if (file.Length <= 0) continue;
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    imageModel.ImageBytes = ms.ToArray();
+                    imageModel.ImageTitle = file.FileName;
+                    imageModel.IsLogo = fileUploadApi.IsLogo;
+                    yield return imageModel;
+                }
+
             }
             
         }
-        private static byte[] FileToByteArray(string fileName)
-        {
-            byte[] fileData = null;
-
-            using (FileStream fs = File.OpenRead(fileName))
-            {
-                var binaryReader = new BinaryReader(fs);
-                fileData = binaryReader.ReadBytes((int)fs.Length);
-            }
-            return fileData;
-        }
+       
     }
 }
