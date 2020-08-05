f9a787475b5c464bafd7dcb3edc4584d836facb5
baouche iqbal <iqbal.baouche@univ-dbkm.dz>
2020-08-05 17:39:16 +0100

    add

--- src/Libraries/SmartRestaurant.Application/Zones/Commands/DeleteZoneCommand.cs
+++ src/Libraries/SmartRestaurant.Application/Zones/Commands/DeleteZoneCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentValidation;
 using MediatR;
 
 namespace SmartRestaurant.Application.Zones.Commands
@@ -6,5 +7,13 @@ namespace SmartRestaurant.Application.Zones.Commands
     public class DeleteZoneCommand : IRequest
     {
         public Guid ZoneId { get; set; }
+    }
+
+    public class DeleteZoneCommandValidator : AbstractValidator<DeleteZoneCommand>
+    {
+        public DeleteZoneCommandValidator()
+        {
+            RuleFor(v => v.ZoneId).Must(val => val != Guid.Empty);
+        }
     }
 }
