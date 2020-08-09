2a88b990fc53128a484a547b7f77ab6ef9d677d4
baouche iqbal <iqbal.baouche@univ-dbkm.dz>
2020-08-09 18:38:32 +0100

    add

---
+++ src/Libraries/SmartRestaurant.Application/Tables/Commands/DeleteTableCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MediatR;
+
+namespace SmartRestaurant.Application.Tables.Commands
+{
+    public class DeleteTableCommand :IRequest
+    {
+        public Guid TableId { get; set; }
+    }
+}
