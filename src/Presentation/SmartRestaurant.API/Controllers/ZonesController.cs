3dcaed107854a68af424dd90765a7e40d965baea
baouche iqbal <iqbal.baouche@univ-dbkm.dz>
2020-08-10 10:20:40 +0100

    [fix]

--- src/Presentation/SmartRestaurant.API/Controllers/ZonesController.cs
+++ src/Presentation/SmartRestaurant.API/Controllers/ZonesController.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SmartRestaurant.API.Helpers;
 using SmartRestaurant.API.Models;
