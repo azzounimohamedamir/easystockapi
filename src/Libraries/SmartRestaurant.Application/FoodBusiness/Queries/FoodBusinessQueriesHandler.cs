f3f66b811af04128b6f9fc9566a00f00015420d9
baouche iqbal <iqbal.baouche@univ-dbkm.dz>
2020-08-24 11:18:57 +0100

    fix

--- src/Libraries/SmartRestaurant.Application/FoodBusiness/Queries/FoodBusinessQueriesHandler.cs
+++ src/Libraries/SmartRestaurant.Application/FoodBusiness/Queries/FoodBusinessQueriesHandler.cs
@@ -34,7 +34,9 @@ namespace SmartRestaurant.Application.FoodBusiness.Queries
             foreach (var foodBusinessDto in data)
             {
                 var images =await _context.FoodBusinessImages.Where(x => x.EntityId == foodBusinessDto.FoodBusinessId).Select(x=>x.ImageBytes).ToListAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
-                foodBusinessDto.Images.AddRange(images.Select(Convert.ToBase64String));
+                
+                if(images.Any())
+                    foodBusinessDto.Images.AddRange(images.Select(Convert.ToBase64String));
             }
             var pagedResult = new PagedListDto<FoodBusinessDto>(result.CurrentPage, result.PageCount, result.PageSize, result.RowCount, data);
             return pagedResult;
