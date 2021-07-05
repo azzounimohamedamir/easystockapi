380f99ae5428cd0a6d0dd0af73f87774195901f1
Aissa Mahmoudbacha <ing_aissa@outlook.com>
2021-07-05 13:44:44 +0100

    [591] Create Reservation : Database Migration

---
+++ src/Libraries/SmartRestaurant.Infrastructure/Migrations/20210705002509_AddReservationTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartRestaurant.Infrastructure.Migrations
+{
+    public partial class AddReservationTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    ReservationId = table.Column<Guid>(nullable: false),
+                    ClientName = table.Column<string>(nullable: true),
+                    NumberOfDiners = table.Column<int>(nullable: false),
+                    ReservationDate = table.Column<DateTime>(nullable: false),
+                    FoodBusinessId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.ReservationId);
+                    table.ForeignKey(
+                        name: "FK_Reservations_FoodBusinesses_FoodBusinessId",
+                        column: x => x.FoodBusinessId,
+                        principalTable: "FoodBusinesses",
+                        principalColumn: "FoodBusinessId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_FoodBusinessId",
+                table: "Reservations",
+                column: "FoodBusinessId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+        }
+    }
+}
