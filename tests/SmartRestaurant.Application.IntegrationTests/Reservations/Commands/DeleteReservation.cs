1fbd4d29f80156b2b7d3d59427f295fefe228ada
سليم زكور فرحات <SalimZekkoure@hotmail.com>
2021-07-08 19:02:46 +0100

    [595] Delete Reservation : Implement  (Tests)

---
+++ tests/SmartRestaurant.Application.IntegrationTests/Reservations/Commands/DeleteReservation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.FoodBusiness.Commands;
+using SmartRestaurant.Application.Reservations.Commands;
+using SmartRestaurant.Domain.Entities;
+
+namespace SmartRestaurant.Application.IntegrationTests.Reservations.Commands
+{
+    using static Testing;
+    [TestFixture]
+    public class DeleteReservation : TestBase
+    {
+       
+        [Test]
+        public async Task DeleteReservation_ShouldSaveToDB()
+        {
+            var createFoodBusinessCommand = new CreateFoodBusinessCommand
+            {
+                FoodBusinessAdministratorId = Guid.NewGuid().ToString(),
+                Name = "fast food test"
+            };
+            await SendAsync(createFoodBusinessCommand);
+            var fastFood = await FindAsync<Domain.Entities.FoodBusiness>(createFoodBusinessCommand.CmdId);
+
+            var createReservationCommand = new CreateReservationCommand
+            {
+                ClientName ="Salim",
+                NumberOfDiners=9,
+                ReservationDate=DateTime.Now.AddDays(1),
+                FoodBusinessId = fastFood.FoodBusinessId,
+            };
+            await SendAsync(createReservationCommand);
+            var reservation = await FindAsync<Domain.Entities.Reservation>(createReservationCommand.CmdId);
+
+            await SendAsync(new DeleteReservationCommand
+            {
+                ReservationId = createReservationCommand.CmdId
+            });
+            var item = await FindAsync<Reservation>(reservation.ReservationId);
+            item.Should().BeNull();
+        }
+    }
+}
---
+++ tests/SmartRestaurant.Application.Tests/CommandValidatorTests/Reservations/DeleteReservationCommandValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.TestHelper;
+using SmartRestaurant.Application.Reservations.Commands;
+using System;
+using Xunit;
+
+namespace SmartRestaurant.Application.Tests.CommandValidatorTests.Reservations
+{
+    public class DeleteReservationCommandValidatorTests
+    {
+        private readonly DeleteReservationCommandValidator _validator;
+
+        public DeleteReservationCommandValidatorTests()
+        {
+            _validator = new DeleteReservationCommandValidator();
+        }
+
+        [Fact]
+        public void Given_EmptyGuid_WhenValidating_ShouldError()
+        {
+            var EmptyGuid = Guid.Empty;
+            _validator.ShouldHaveValidationErrorFor(restaurant => restaurant.ReservationId, EmptyGuid);
+        }
+    }
+}
