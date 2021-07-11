7c007cac2c572040732b03bf7cba58bb3d617a63
Aissa Mahmoudbacha <ing_aissa@outlook.com>
2021-07-11 08:02:26 +0100

    [599] List Reservations : I have Implemented Integration Tests (Tests).

---
+++ tests/SmartRestaurant.Application.IntegrationTests/Reservations/Queries/GetReservationsListByReservationDateTimeIntervalTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.FoodBusiness.Commands;
+using SmartRestaurant.Application.Menus.Commands;
+using SmartRestaurant.Application.Reservations.Commands;
+using SmartRestaurant.Application.Sections.Commands;
+using SmartRestaurant.Application.Sections.Queries;
+using SmartRestaurant.Domain.Enums;
+
+namespace SmartRestaurant.Application.IntegrationTests.Sections.Queries
+{
+    using static Testing;
+
+    [TestFixture]
+    public class GetReservationsListByReservationDateTimeIntervalTest : TestBase
+    {
+        [Test]
+        public async Task ShouldGetReservationsList_ByReservationDateTimeInterval()
+        {
+            //Create a FoodBusiness
+            var createFoodBusinessCommand = new CreateFoodBusinessCommand
+            {
+                FoodBusinessAdministratorId = Guid.NewGuid().ToString(),
+                Name = "fast food test"
+            };
+            await SendAsync(createFoodBusinessCommand);
+            var fastFood = await FindAsync<Domain.Entities.FoodBusiness>(createFoodBusinessCommand.CmdId);
+
+            //Create Reservations
+            DateTime dateTimeNow = DateTime.Now;
+            for (int i = 1; i <= 5; i++)
+            {
+                
+                var createReservationCommand = new CreateReservationCommand
+                {
+                    ReservationName = $"Aissa_{i}",
+                    NumberOfDiners = 3 + i,
+                    ReservationDate = dateTimeNow.AddHours(i),
+                    FoodBusinessId = fastFood.FoodBusinessId,
+                    CreatedBy = Guid.NewGuid().ToString()
+                };
+                await SendAsync(createReservationCommand);
+            }
+
+            var query = new GetReservationsListByReservationDateTimeIntervalQuery
+            {
+                FoodBusinessId = Guid.NewGuid(),
+                TimeIntervalStart = dateTimeNow,
+                TimeIntervalEnd = dateTimeNow.AddHours(5),
+                Page = 0,
+                PageSize = 5
+            };            
+            var result = await SendAsync(query);
+            result.Data.Should().HaveCount(0);
+
+
+            var query_00 = new GetReservationsListByReservationDateTimeIntervalQuery
+            {
+                FoodBusinessId = fastFood.FoodBusinessId,
+                TimeIntervalStart = dateTimeNow,
+                TimeIntervalEnd = dateTimeNow.AddHours(5),
+                Page = 1,
+                PageSize = 5
+            };
+            var result_00 = await SendAsync(query_00);
+            result_00.Data.Should().HaveCount(5);
+
+
+            var query_01 = new GetReservationsListByReservationDateTimeIntervalQuery
+            {
+                FoodBusinessId = fastFood.FoodBusinessId,
+                TimeIntervalStart = dateTimeNow,
+                TimeIntervalEnd = dateTimeNow.AddHours(5),
+                Page = 2,
+                PageSize = 5
+            };
+            var result_01 = await SendAsync(query_01);
+            result_01.Data.Should().HaveCount(0);
+
+
+            var query_02 = new GetReservationsListByReservationDateTimeIntervalQuery
+            {
+                FoodBusinessId = fastFood.FoodBusinessId,
+                TimeIntervalStart = dateTimeNow.AddHours(3),
+                TimeIntervalEnd = dateTimeNow.AddHours(5),
+                Page = 1,
+                PageSize = 2
+            };
+            var result_02 = await SendAsync(query_02);
+            result_02.Data.Should().HaveCount(2);
+
+
+            var query_03 = new GetReservationsListByReservationDateTimeIntervalQuery
+            {
+                FoodBusinessId = fastFood.FoodBusinessId,
+                TimeIntervalStart = dateTimeNow.AddHours(3),
+                TimeIntervalEnd = dateTimeNow.AddHours(5),
+                Page = 2,
+                PageSize = 2
+            };
+            var result_03 = await SendAsync(query_03);
+            result_03.Data.Should().HaveCount(1);
+
+
+            var query_04 = new GetReservationsListByReservationDateTimeIntervalQuery
+            {
+                FoodBusinessId = fastFood.FoodBusinessId,
+                TimeIntervalStart = dateTimeNow.AddHours(3),
+                TimeIntervalEnd = dateTimeNow.AddHours(5),
+                Page = 3,
+                PageSize = 2
+            };
+            var result_04 = await SendAsync(query_04);
+            result_04.Data.Should().HaveCount(0);
+        }
+    }
+}
