a3ea5cca7a4b0dcadd12f7441ab743d5b1156677
Aissa Mahmoudbacha <ing_aissa@outlook.com>
2021-07-12 12:56:53 +0100

    [606] Get Client Non Expired Reservations : Code refactoring and cleaning

--- tests/SmartRestaurant.Application.IntegrationTests/Reservations/Queries/GetClientReservationsHistoryTest.cs
+++ tests/SmartRestaurant.Application.IntegrationTests/Reservations/Queries/GetClientReservationsHistoryTest.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
-using SmartRestaurant.Application.FoodBusiness.Commands;
-using SmartRestaurant.Application.Reservations.Commands;
+using SmartRestaurant.Application.IntegrationTests.TestTools;
 using SmartRestaurant.Application.Reservations.Queries;
 
 namespace SmartRestaurant.Application.IntegrationTests.Reservations.Queries
@@ -17,17 +15,17 @@ namespace SmartRestaurant.Application.IntegrationTests.Reservations.Queries
         [Test]
         public async Task ShouldGetClientReservationsHistory()
         {
-            var fastFood = await CreateFoodBusiness();
+            var fastFood = await FoodBusinessTestTools.CreateFoodBusiness();
 
             string client_01_UserId = Guid.NewGuid().ToString();
             string client_02_UserId = Guid.NewGuid().ToString();
 
-            await Create_5_NonExpiredReservations(fastFood,"Aissa", client_01_UserId);
-            await Create_3_ExpiredReservations(fastFood, "Aissa", client_01_UserId);
+            await ReservationsTestTools.Create_5_NonExpiredReservations(fastFood, "Aissa", client_01_UserId);
+            await ReservationsTestTools.Create_3_ExpiredReservations(fastFood, "Aissa", client_01_UserId);
 
-            await Create_5_NonExpiredReservations(fastFood, "Bilel", client_02_UserId);
-            await Create_3_ExpiredReservations(fastFood, "Bilel", client_02_UserId);
-         
+            await ReservationsTestTools.Create_5_NonExpiredReservations(fastFood, "Bilel", client_02_UserId);
+            await ReservationsTestTools.Create_3_ExpiredReservations(fastFood, "Bilel", client_02_UserId);
+
             var client_01_query_00 = new GetClientReservationsHistoryQuery
             {
                 CreatedBy = client_01_UserId,
@@ -76,54 +74,6 @@ namespace SmartRestaurant.Application.IntegrationTests.Reservations.Queries
             };
             var client_01_result_04 = await SendAsync(client_01_query_04);
             client_01_result_04.Data.Should().HaveCount(0);
-        }
-
-        private static async Task Create_5_NonExpiredReservations(Domain.Entities.FoodBusiness fastFood, string reservationName, string client_UserId)
-        {
-            DateTime dateTimeNow = DateTime.Now;
-            for (int i = 1; i <= 5; i++)
-            {
-                var createReservationCommand = new CreateReservationCommand
-                {
-                    ReservationName = $"{reservationName}_NonExpiredReservations_{i}",
-                    NumberOfDiners = 3 + i,
-                    ReservationDate = dateTimeNow.AddHours(i),
-                    FoodBusinessId = fastFood.FoodBusinessId,
-                    CreatedBy = client_UserId
-                };
-                await SendAsync(createReservationCommand);
-            }
-        }
-
-        private static async Task Create_3_ExpiredReservations(Domain.Entities.FoodBusiness fastFood,string reservationName,  string client_UserId)
-        {
-            DateTime dateTimeNow = DateTime.Now;
-           
-            for (int i = 1; i <= 3; i++)
-            {
-                var createReservationCommand = new CreateReservationCommand
-                {
-                    ReservationName = $"{reservationName}_ExpiredReservations_{i}",
-                    NumberOfDiners = 1 + i,
-                    ReservationDate = dateTimeNow.AddMilliseconds(i*500),
-                    FoodBusinessId = fastFood.FoodBusinessId,
-                    CreatedBy = client_UserId
-                };
-                await SendAsync(createReservationCommand);               
-            }
-            await Task.Delay(3500);
-        }
-
-        private static async Task<Domain.Entities.FoodBusiness> CreateFoodBusiness()
-        {
-            var createFoodBusinessCommand = new CreateFoodBusinessCommand
-            {
-                FoodBusinessAdministratorId = Guid.NewGuid().ToString(),
-                Name = "fast food test"
-            };
-            await SendAsync(createFoodBusinessCommand);
-            var fastFood = await FindAsync<Domain.Entities.FoodBusiness>(createFoodBusinessCommand.CmdId);
-            return fastFood;
         }
     }
 }
