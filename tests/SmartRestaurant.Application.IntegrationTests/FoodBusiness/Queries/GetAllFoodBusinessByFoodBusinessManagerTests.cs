bb1d1d1cfae2cee15908d18b21d1e1ff3d440373
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-07-22 15:41:42 +0200

    I have implemented integration tests.

---
+++ tests/SmartRestaurant.Application.IntegrationTests/FoodBusiness/Queries/GetAllFoodBusinessByFoodBusinessManagerTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.FoodBusiness.Commands;
+using SmartRestaurant.Application.FoodBusiness.Queries;
+using SmartRestaurant.Domain.Entities;
+
+namespace SmartRestaurant.Application.IntegrationTests.FoodBusiness.Queries
+{
+    using static Testing;
+
+    
+
+    public class GetAllFoodBusinessByFoodBusinessManagerTests : TestBase
+    {
+        [Test]
+        public async Task ShouldReturnAllFoodBusinessAssignedToFoodBusinessManagerTests()
+        {
+            var tacosDzFoodBusinessAdministratorId = Guid.NewGuid().ToString();
+            var tacosDz1 = new CreateFoodBusinessCommand
+            {
+                Name = "tacos Dz 01",
+                FoodBusinessAdministratorId = tacosDzFoodBusinessAdministratorId,
+                AverageRating = 12,
+                HasCarParking = true
+            };
+            var tacosDz1FoodBusinessId = tacosDz1.CmdId;
+            await SendAsync(tacosDz1);
+
+            var tacosDz2 = new CreateFoodBusinessCommand
+            {
+                Name = "tacos Dz 02",
+                FoodBusinessAdministratorId = tacosDzFoodBusinessAdministratorId,
+                AverageRating = 10,
+                HasCarParking = false
+            };
+            var tacosDz2FoodBusinessId = tacosDz2.CmdId;
+            await SendAsync(tacosDz2);
+
+            var tacosDz3 = new CreateFoodBusinessCommand
+            {
+                Name = "tacos Dz 03",
+                FoodBusinessAdministratorId = tacosDzFoodBusinessAdministratorId,
+                AverageRating = 10,
+                HasCarParking = false
+            };
+            var tacosDz3FoodBusinessId = tacosDz3.CmdId;
+            await SendAsync(tacosDz3);
+
+            var BigMamaFoodBusinessAdministratorId= Guid.NewGuid().ToString();
+            var bigMama = new CreateFoodBusinessCommand
+            {
+                Name = "bigMama Dz",
+                FoodBusinessAdministratorId = BigMamaFoodBusinessAdministratorId,
+                AverageRating = 10,
+                HasCarParking = false
+            };
+            var bigMamaFoodBusinessId = bigMama.CmdId;
+            await SendAsync(bigMama);
+
+
+            var aissaFoodBusinessManager = _authenticatedUserId;
+            var assignAissaFoodBusinessManagerToTacosDz1 = new FoodBusinessUser
+            {
+                ApplicationUserId = aissaFoodBusinessManager,
+                FoodBusinessId = tacosDz1FoodBusinessId,
+            };
+            await AddAsync(assignAissaFoodBusinessManagerToTacosDz1);
+
+            var assignAissaFoodBusinessManagerToTacosDz2 = new FoodBusinessUser
+            {
+                ApplicationUserId = aissaFoodBusinessManager,
+                FoodBusinessId = tacosDz2FoodBusinessId,
+            };
+            await AddAsync(assignAissaFoodBusinessManagerToTacosDz2);
+
+            var karimFoodBusinessManager = Guid.NewGuid().ToString();
+            var assignKarimFoodBusinessManagerToTacosDz3 = new FoodBusinessUser
+            {
+                ApplicationUserId = karimFoodBusinessManager,
+                FoodBusinessId = tacosDz3FoodBusinessId,
+            };
+            await AddAsync(assignKarimFoodBusinessManagerToTacosDz3);
+
+
+            var ahmedFoodBusinessManager = Guid.NewGuid().ToString();
+            var assignAhmedFoodBusinessManagerToBigMama = new FoodBusinessUser
+            {
+                ApplicationUserId = ahmedFoodBusinessManager,
+                FoodBusinessId = bigMamaFoodBusinessId,
+            };
+            await AddAsync(assignAhmedFoodBusinessManagerToBigMama);
+
+
+
+            var result = await SendAsync(new GetAllFoodBusinessByFoodBusinessManagerQuery { });
+
+            result.Should().HaveCount(2);
+        }
+    }
+}
