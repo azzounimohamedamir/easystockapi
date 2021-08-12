5cbcff1a002570c206769e03cdfb9b966c4f86ad
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-08-12 19:44:25 +0200

    I have fixed merge conflict

---
+++ src/Libraries/SmartRestaurant.Application/Common/Dtos/FoodBusinessEmployeesDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmartRestaurant.Application.Common.Dtos
+{
+    public class FoodBusinessEmployees
+    {
+
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public List<string> Roles { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool isActive { get; set; }
+        public Guid FoodBusinessId { get; set; }
+        public Guid FoodBusinessName { get; set; }
+    }
+}
---
+++ src/Libraries/SmartRestaurant.Application/Common/constants/Roles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartRestaurant.Application.Common.constants
+{
+    public class Roles
+    {
+       public static string SuperAdmin = "SuperAdmin";
+       public static string SupportAgent = "SupportAgent";
+       public static string SalesMan = "SalesMan";
+       public static string Photograph = "Photograph";
+       public static string FoodBusinessAdministrator = "FoodBusinessAdministrator";
+       public static string FoodBusinessManager = "FoodBusinessManager";
+       public static string FoodBusinessOwner = "FoodBusinessManager";
+       public static string Chef = "Chef";
+       public static string Cashier = "Cashier";
+       public static string Waiter = "Waiter";
+       public static string Diner = "Diner";
+       public static string  Anounymous = "Anounymous";
+       public static string Organization = "Organization";
+    }
+}
