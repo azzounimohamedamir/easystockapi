71d446fc3dc879fe31af781b74c3f1c1d5a06d6a
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-08-16 04:56:20 +0200

    I have added SMTP configuration.

---
+++ src/Libraries/SmartRestaurant.Application/Common/Tools/EmailHelper.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SmartRestaurant.Application.Common.Tools
+{
+    public class SmtpConfig
+    {
+        public string Server { get; set; }
+        public string Email { get; set; }
+        public string Pass { get; set; }
+        public int Port { get; set; }
+    }
+}
