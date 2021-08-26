6515bddf6300f798855be1e3fcf3c867e54c44b1
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-08-26 17:08:30 +0200

    [629] Backend-ReturnListOfCountries : I have created the handler used to return the list of countries.

---
+++ src/Libraries/SmartRestaurant.Application/Resources/Queries/GetListOfCountriesQuery.cs
@@ -0,0 +1,18 @@
+﻿
+
+using MediatR;
+
+namespace SmartRestaurant.Application.Resources.Queries
+{
+    public class GetListOfCountriesQuery : IRequest<string>
+    {
+    }
+
+    public class Countries
+    {
+        public string Name { get; set; }
+        public string DialCode { get; set; }
+        public string IsoCode { get; set; }
+        public string Flag { get; set; }
+    }
+}
