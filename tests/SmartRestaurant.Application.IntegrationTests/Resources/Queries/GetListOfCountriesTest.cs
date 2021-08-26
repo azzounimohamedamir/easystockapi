fe829cf172a5a76d57083d7d236a69fbafcfa76b
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-08-26 17:49:38 +0200

    [629] Backend-ReturnListOfCountries : I have implemented integrations tests

---
+++ tests/SmartRestaurant.Application.IntegrationTests/Resources/Queries/GetListOfCountriesTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.Resources.Queries;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SmartRestaurant.Application.IntegrationTests.Resources.Queries
+{
+    using static Testing;
+
+    [TestFixture]
+    public class GetListOfCountriesTest
+    {
+        [Test]
+        public async Task ShouldListOfCountries()
+        {
+            var listOfCountries = await SendAsync(new GetListOfCountriesQuery());
+            listOfCountries.Should().NotBeNull();
+            listOfCountries.Should().NotBeEmpty();
+            listOfCountries.Should().Contain("\"isoCode\": \"DZ\"");
+            listOfCountries.Should().Contain("\"name\"");
+            listOfCountries.Should().Contain("\"dialCode\"");
+            listOfCountries.Should().Contain("\"isoCode\"");
+            listOfCountries.Should().Contain("\"flag\"");
+        }
+    }
+}
