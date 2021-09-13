4dfdd3bbb0060dad2d8f0f5cfae23c152f89499d
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-09-13 03:44:01 +0200

    I have removed unnecessary properties and added a validation to some fields.

--- src/Libraries/SmartRestaurant.Application/FoodBusiness/Commands/CreateFoodBusinessCommand.cs
+++ src/Libraries/SmartRestaurant.Application/FoodBusiness/Commands/CreateFoodBusinessCommand.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using FluentValidation;
 using SmartRestaurant.Application.Common.Commands;
 using SmartRestaurant.Application.Common.Dtos.ValueObjects;
+using SmartRestaurant.Application.Common.Tools;
 using SmartRestaurant.Domain.Enums;
 
 namespace SmartRestaurant.Application.FoodBusiness.Commands
@@ -12,8 +14,6 @@ namespace SmartRestaurant.Application.FoodBusiness.Commands
         public AddressDto Address { get; set; }
         public PhoneNumberDto PhoneNumber { get; set; }
         public string Description { get; set; }
-        public float AverageRating { get; set; }
-        public int NumberRatings { get; set; }
         public bool HasCarParking { get; set; }
         public bool IsHandicapFriendly { get; set; }
         public bool OffersTakeout { get; set; }
@@ -35,8 +35,27 @@ namespace SmartRestaurant.Application.FoodBusiness.Commands
     {
         public CreateFoodBusinessCommandValidator()
         {
-            RuleFor(v => v.Name).MaximumLength(200).NotEmpty();
-            RuleFor(v => v.FoodBusinessAdministratorId).NotEmpty();
+            RuleFor(foodBusiness => foodBusiness.Name)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty()
+                .MaximumLength(200);
+
+            RuleFor(invitedUser => invitedUser.FoodBusinessAdministratorId)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty()
+                .NotEqual(Guid.Empty.ToString())
+                .Must(ValidatorHelper.ValidateGuid).WithMessage("'{PropertyName}' must be a valid GUID");
+
+            RuleFor(foodBusiness => foodBusiness.Email)
+                .Must(ValidatorHelper.ValidateEmail).WithMessage("'{PropertyName}: {PropertyValue}' is invalide")
+                .When(foodBusiness => !String.IsNullOrWhiteSpace(foodBusiness.Email));
+
+            RuleFor(foodBusiness => foodBusiness.Website)
+                .Must(ValidatorHelper.ValidateUrl).WithMessage("'{PropertyName}: {PropertyValue}' is invalide")
+                .When(foodBusiness => !String.IsNullOrWhiteSpace(foodBusiness.Website));
+
+            RuleFor(foodBusiness => foodBusiness.FourDigitCode)
+                .LessThanOrEqualTo(9999);
         }
     }
 }
