32a59c706fd08c0942a0f9b138feba4f057585dd
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-09-20 02:07:40 +0200

    I have added a Swagger Doc to the endpoint used create a new table.

--- src/Presentation/SmartRestaurant.API/Controllers/TablesController.cs
+++ src/Presentation/SmartRestaurant.API/Controllers/TablesController.cs
@@ -2,13 +2,16 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SmartRestaurant.API.Swagger.Exception;
 using SmartRestaurant.Application.Tables.Commands;
 using SmartRestaurant.Application.Tables.Queries;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace SmartRestaurant.API.Controllers
 {
     [Route("api/tables")]
     [ApiController]
+    [SwaggerTag("List of actions that can be applied on FoodBusiness Tables")]
     public class TablesController : ApiController
     {
         [Route("zone/{zoneId:Guid}")]
@@ -27,6 +30,14 @@ namespace SmartRestaurant.API.Controllers
             return await SendWithErrorsHandlingAsync(new GetTableByIdQuery {TableId = id});
         }
 
+        /// <summary> CreateNewTable() </summary>
+        /// <remarks>This endpoint allows us to create a new table within a FoodBusiness zone.</remarks>
+        /// <param name="command">This is payload object used to create a new table</param>
+        /// <response code="204">The new table has been successfully created.</response>
+        /// <response code="400">The payload data sent to the backend-server in order to create a new table is invalid.</response>
+        /// <response code="401">The cause of 401 error is one of two reasons: Either the user is not logged into the application or authentication token is invalid or expired.</response>
+        /// <response code="403">The user account you used to log into the application, does not have the necessary privileges to execute this request.</response>
+        [ProducesResponseType(typeof(ExceptionResponse), 400)]
         [HttpPost]
         [Authorize(Roles = "FoodBusinessManager")]
         public async Task<IActionResult> Create(CreateTableCommand command)
