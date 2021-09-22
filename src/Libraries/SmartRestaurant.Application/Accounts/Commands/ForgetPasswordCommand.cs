1a128d6ada6d5d94387d693d8829124e344ecc2d
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-09-22 23:15:18 +0200

    [663] ForgottenPasswordInTheLoginPage : I have added  forget password handler.

---
+++ src/Libraries/SmartRestaurant.Application/Accounts/Commands/ForgetPasswordCommand.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+using SmartRestaurant.Application.Common.Tools;
+using SmartRestaurant.Application.Common.WebResults;
+
+namespace SmartRestaurant.Application.Accounts.Commands
+{
+    public class ForgetPasswordCommand : IRequest<NoContent>
+    {
+        public string Email { get; set; }
+    }
+
+    public class ForgetPasswordCommandValidator : AbstractValidator<ForgetPasswordCommand>
+    {
+        public ForgetPasswordCommandValidator()
+        {
+            RuleFor(foodBusiness => foodBusiness.Email)
+               .Cascade(CascadeMode.StopOnFirstFailure)
+               .NotEmpty()
+               .MaximumLength(200)
+               .Must(ValidatorHelper.ValidateEmail).WithMessage("'{PropertyName}: {PropertyValue}' is invalide");
+
+        }
+    }
+}
