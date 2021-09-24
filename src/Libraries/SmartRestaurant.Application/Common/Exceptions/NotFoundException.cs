82f7ce05de340721cf850aed23cbdc94ad85ebfb
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-09-24 05:42:51 +0200

    [669] Reset Password : I have fixed NotFoundException

--- src/Libraries/SmartRestaurant.Application/Common/Exceptions/NotFoundException.cs
+++ src/Libraries/SmartRestaurant.Application/Common/Exceptions/NotFoundException.cs
@@ -1,10 +1,12 @@
-﻿namespace SmartRestaurant.Application.Common.Exceptions
+﻿using System.Collections.Generic;
+
+namespace SmartRestaurant.Application.Common.Exceptions
 {
     public class NotFoundException : BaseException
     {
-        public NotFoundException(string name, object key)
-            : base(404, $"Entity \"{name}\" ({key}) was not found.")
+        public NotFoundException(string name, object key) : base(404, "Not found error")
         {
+            Errors = new List<string> { $"Entity \"{name}\" ({key}) was not found." };
         }
     }
 }
