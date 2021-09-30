94e50a5d7afb83d4b2a0da87a11dc722caffab10
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-09-30 20:18:02 +0200

    [680] FoodBusinessManagerDisplayMenuSubSectionDetails : I have added the logic part that will be used to fetch sub-section details.

---
+++ src/Libraries/SmartRestaurant.Application/SubSections/Queries/GetSubSectionByIdQuery.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using Newtonsoft.Json;
+using SmartRestaurant.Application.Common.Dtos;
+using SmartRestaurant.Application.Common.Tools;
+using System;
+
+namespace SmartRestaurant.Application.SubSections.Queries
+{
+    public class GetSubSectionByIdQuery : IRequest<SubSectionDto>
+    {
+        [JsonIgnore] public string Id { get; set; }
+    }
+
+    public class GetSubSectionByIdQueryValidator : AbstractValidator<GetSubSectionByIdQuery>
+    {
+        public GetSubSectionByIdQueryValidator()
+        {
+            RuleFor(section => section.Id)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty()
+                .NotEqual(Guid.Empty.ToString())
+                .Must(ValidatorHelper.ValidateGuid).WithMessage("'{PropertyName}' must be a valid GUID");
+        }
+    }
+}
