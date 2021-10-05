5a5ba55c0852bad8f9f33fa0ec7a22b0cada7c66
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-10-05 15:01:11 +0200

    [704] SM-UserCreateProduct : I have implemented unit tests.

---
+++ tests/SmartRestaurant.Application.Tests/MappingTests/ProductMappingTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using AutoMapper;
+using SmartRestaurant.Application.Products.Commands;
+using SmartRestaurant.Application.Tests.Configuration;
+using SmartRestaurant.Domain.Entities;
+using Xunit;
+
+namespace SmartRestaurant.Application.Tests.MappingTests
+{
+    public class ProductMappingTest : IClassFixture<MappingTestsFixture>
+    {
+        private readonly IMapper _mapper;
+
+        public ProductMappingTest(MappingTestsFixture fixture)
+        {
+            _mapper = fixture.Mapper;
+        }
+
+
+        [Fact]
+        public void Map_CreateProductCommand_To_Product_Valide_Test()
+        {
+            var createProductCommand = new CreateProductCommand
+            {
+                Name = "hamoud 2L",
+                Description = "description test",
+                Price = 150,
+                EnergeticValue = 200,
+                SectionId = Guid.NewGuid().ToString(),
+                SubSectionId = Guid.NewGuid().ToString()
+            };
+
+
+            var product = _mapper.Map<Product>(createProductCommand);
+            Assert.Equal(product.ProductId, createProductCommand.Id);
+            Assert.Equal(product.Name, createProductCommand.Name);
+            Assert.Equal(product.Description, createProductCommand.Description);
+            Assert.Equal(product.Price, createProductCommand.Price);
+            Assert.Equal(product.EnergeticValue, createProductCommand.EnergeticValue);
+            Assert.Equal(product.SectionId, Guid.Parse(createProductCommand.SectionId));
+            Assert.Equal(product.SubSectionId, Guid.Parse(createProductCommand.SubSectionId));
+            Assert.Null(product.Picture);
+
+        }
+    }
+}
