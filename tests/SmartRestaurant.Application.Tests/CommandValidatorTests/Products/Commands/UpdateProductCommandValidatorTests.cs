e9818345b7520f7d4b33081c02cc49fcf74fd933
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-10-05 16:21:02 +0200

    [708] SM-UserDeleteProduct : I have implemented unit tests.

--- tests/SmartRestaurant.Application.Tests/CommandValidatorTests/Products/Commands/UpdateProductCommandValidatorTests.cs
+++ tests/SmartRestaurant.Application.Tests/CommandValidatorTests/Products/Commands/UpdateProductCommandValidatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation.TestHelper;
 using Microsoft.AspNetCore.Http;
 using SmartRestaurant.Application.Products.Commands;
+using System;
 using Xunit;
 
 namespace SmartRestaurant.Application.Tests.CommandValidatorTests.Products.Commands
@@ -12,6 +13,41 @@ namespace SmartRestaurant.Application.Tests.CommandValidatorTests.Products.Comma
         public UpdateProductCommandValidatorTests()
         {
             _validator = new UpdateProductCommandValidator();
+        }
+
+        [Fact]
+        public void Given_IdIsNull_WhenValidating_ShouldGetAnError()
+        {
+            string _null = null;
+            _validator.ShouldHaveValidationErrorFor(x => x.Id, _null);
+        }
+
+        [Fact]
+        public void Given_IdIsEmpty_WhenValidating_ShouldGetAnError()
+        {
+            var empty = "";
+            _validator.ShouldHaveValidationErrorFor(x => x.Id, empty);
+        }
+
+        [Fact]
+        public void Given_IdIsEmptyGuid_WhenValidating_ShouldGetAnError()
+        {
+            var emptyGuid = Guid.Empty.ToString();
+            _validator.ShouldHaveValidationErrorFor(x => x.Id, emptyGuid);
+        }
+
+        [Fact]
+        public void Given_IdIsInvalidGuid_WhenValidating_ShouldGetAnError()
+        {
+            var invalidGuid = "3cbf3570-4444-4673-8746-2";
+            _validator.ShouldHaveValidationErrorFor(x => x.Id, invalidGuid);
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIsValid_WhenValidating_ShouldBeValidated()
+        {
+            var validEntry = "acbf657b-3398-7a73-8746-77b7cf596315";
+            _validator.ShouldNotHaveValidationErrorFor(x => x.Id, validEntry);
         }
 
         [Fact]
