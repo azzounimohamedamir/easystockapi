e5b69d53d4f398f897420b97aeb1ab95dad49856
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-10-06 23:10:59 +0200

    [712] SM-UserListProduct : I have implemented unit tests.

---
+++ tests/SmartRestaurant.Application.Tests/CommandValidatorTests/Products/Queries/GetProductListQueryValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.TestHelper;
+using SmartRestaurant.Application.Products.Queries;
+using Xunit;
+
+namespace SmartRestaurant.Application.Tests.CommandValidatorTests.Products.Commands
+{
+    public class GetProductListQueryValidatorTests
+    {
+        private readonly GetProductListQueryValidator _validator;
+
+        public GetProductListQueryValidatorTests()
+        {
+            _validator = new GetProductListQueryValidator();
+        }
+
+        [Fact]
+        public void Given_PageSizeExceedMax_WhenValidating_ShouldGetAnError()
+        {
+            int exceedMax = 101;
+            _validator.ShouldHaveValidationErrorFor(x => x.PageSize, exceedMax);
+        }       
+    }
+}
