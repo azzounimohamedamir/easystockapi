c39919be9d8a3fc91913936cf45f9d72c7788f80
Khaoula Bouzekria <khaoulabouzekria@outlook.com>
2021-10-06 09:18:13 +0100

    -Create FoodBusinessClientUpdateCommand

--- src/Libraries/SmartRestaurant.Application/FoodBusinessClient/Commands/FoodBusinessClientCommandHandler.cs
+++ src/Libraries/SmartRestaurant.Application/FoodBusinessClient/Commands/FoodBusinessClientCommandHandler.cs
@@ -1,17 +1,21 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using SmartRestaurant.Application.Common.Exceptions;
 using SmartRestaurant.Application.Common.Interfaces;
 using SmartRestaurant.Application.Common.WebResults;
 using SmartRestaurant.Domain.Identity.Entities;
-using System.Threading;
-using System.Threading.Tasks;
+
 
 namespace SmartRestaurant.Application.FoodBusinessClient.Commands
 {
     public class FoodBusinessClientCommandHandler :
-        IRequestHandler<CreateFoodBusinessClientCommand, Created>
+        IRequestHandler<CreateFoodBusinessClientCommand, Created>,
+        IRequestHandler<UpdateFoodBusinessClientCommand, NoContent>
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
@@ -37,6 +41,24 @@ namespace SmartRestaurant.Application.FoodBusinessClient.Commands
             var entity = _mapper.Map<Domain.Entities.FoodBusinessClient>(request);
             _context.FoodBusinessClients.Add(entity);
             await _context.SaveChangesAsync(cancellationToken);
+            return default;
+        }
+        public async Task<NoContent> Handle(UpdateFoodBusinessClientCommand request,
+           CancellationToken cancellationToken)
+        {
+            var validator = new UpdateFoodBusinessClientCommandValidator();
+            var result = await validator.ValidateAsync(request, cancellationToken).ConfigureAwait(false);
+            if (!result.IsValid) throw new ValidationException(result);
+
+            var FoodBusinessClients = await _context.FoodBusinessClients.AsNoTracking()
+                .FirstOrDefaultAsync(FoodBusinessClients => FoodBusinessClients.FoodBusinessClientId == Guid.Parse(request.Id), cancellationToken)
+                .ConfigureAwait(false);
+            if (FoodBusinessClients == null)
+                throw new NotFoundException(nameof(FoodBusinessClient), request.Id);
+
+            _mapper.Map(request, FoodBusinessClients);
+            _context.FoodBusinessClients.Update(FoodBusinessClients);
+            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
             return default;
         }
     }
--- src/Presentation/SmartRestaurant.API/Controllers/FoodBusinessClientController.cs
+++ src/Presentation/SmartRestaurant.API/Controllers/FoodBusinessClientController.cs
@@ -16,5 +16,14 @@ namespace SmartRestaurant.API.Controllers
         {
             return await SendWithErrorsHandlingAsync(command);
         }
+
+        [Route("{id}")]
+        [HttpPut]
+        [Authorize(Roles = "FoodBusinessManager,FoodBusinessAdministrator,SupportAgent,SuperAdmin")]
+        public async Task<IActionResult> Update([FromRoute] string id, UpdateFoodBusinessClientCommand command)
+        {
+            command.Id = id;
+            return await SendWithErrorsHandlingAsync(command);
+        }
     }
 }
