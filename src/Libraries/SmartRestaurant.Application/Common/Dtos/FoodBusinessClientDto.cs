b0047dc434c4638a75c7a9e4ff6b0a57f9459863
Khaoula Bouzekria <khaoulabouzekria@outlook.com>
2021-10-07 12:46:08 +0100

    -Add FoodBusinessClient Dto.

---
+++ src/Libraries/SmartRestaurant.Application/Common/Dtos/FoodBusinessClientDto.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using SmartRestaurant.Application.Common.Dtos.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartRestaurant.Application.Common.Dtos
+{
+    public class FoodBusinessClientDto
+    {
+        [JsonProperty(PropertyName = "id")] public Guid FoodBusinessClientId { get; set; }
+        public string Name { get; set; }
+        public int NRC { get; set; }
+        public int NIF { get; set; }
+        public int NIS { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public AddressDto Address { get; set; }
+        public PhoneNumberDto PhoneNumber { get; set; }
+        public string Website { get; set; }
+        public string ManagerId { get; set; }
+    }
+}
