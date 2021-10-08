6294a371f3f96da69c849418a8a40bc2c8f3c397
Khaoula Bouzekria <khaoulabouzekria@outlook.com>
2021-10-08 17:00:04 +0100

    -Add GetFoodBusinessClientById Tests

---
+++ tests/SmartRestaurant.Application.IntegrationTests/FoodBusinessClient/Queries/GetFoodBusinessClientByIdTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using SmartRestaurant.Application.FoodBusinessClient.Queries;
+using SmartRestaurant.Application.IntegrationTests.TestTools;
+
+namespace SmartRestaurant.Application.IntegrationTests.FoodBusinessClient.Queries
+{
+    using static Testing;
+
+    class GetFoodBusinessClientByIdTests : TestBase
+    {
+        [Test]
+        public async Task ShouldGetFoodBusinessClient_ById()
+        {
+            await RolesTestTools.CreateRoles();
+            var foodBusinessClientManager = await UsersTestTools.CreateFoodBusinessClientManager();
+            var fastFood = await FoodBusinessClientTestTools.CreateFoodBusinessClient(foodBusinessClientManager.Id);
+            var query = new GetFoodBusinessClientByIdQuery 
+            { 
+                FoodBusinessClientId = fastFood.FoodBusinessClientId 
+            };
+            var result = await SendAsync(query);
+            result.Name.Should().Be("G22 REI");
+        }
+    }
+}
