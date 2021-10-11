37633d4c17b29b9a25442ef19beaf59d655b4ebe
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-10-11 07:29:39 +0200

    I have created a filter strategy

---
+++ src/Libraries/SmartRestaurant.Application/Ingredients/Queries/FilterStrategy/IIngredientFilterStrategy.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartRestaurant.Application.Common.Extensions;
+using SmartRestaurant.Application.Ingredients.Queries;
+using SmartRestaurant.Domain.Entities;
+
+namespace SmartRestaurant.Application.Products.Queries.FilterStrategy
+{
+    public interface IIngredientFilterStrategy
+    {     
+        public PagedResultBase<Ingredient> FetchData(DbSet<Ingredient> ingredients, GetIngredientsListQuery request);
+    }
+}
