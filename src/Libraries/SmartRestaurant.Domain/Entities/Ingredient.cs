4b249602e07a66ccd5a1dda7bdfda55a91b4a008
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-10-11 05:52:24 +0200

    I have updated Ingredient entity

--- src/Libraries/SmartRestaurant.Domain/Entities/Ingredient.cs
+++ src/Libraries/SmartRestaurant.Domain/Entities/Ingredient.cs
@@ -7,14 +7,8 @@ namespace SmartRestaurant.Domain.Entities
     public class Ingredient : AuditableEntity
     {
         public Guid IngredientId { get; set; }
-        public string Name { get; set; }
-        public float Fat { get; set; }
-        public float Protein { get; set; }
-        public float Carbs { get; set; }
-        public float Energy { get; set; }
-        public Quantity MinQuantity { get; set; }
-        public Quantity MaxQuantity { get; set; }
-
-        public Guid FoodBusinessId { get; set; }
+        public string Names { get; set; }
+        public byte[] Picture { get; set; }
+        public EnergeticValue EnergeticValue { get; set; }
     }
 }
---
+++ src/Libraries/SmartRestaurant.Domain/ValueObjects/EnergeticValue.cs
@@ -0,0 +1,30 @@
+﻿
+using SmartRestaurant.Domain.Common;
+using SmartRestaurant.Domain.Enums;
+using System.Collections.Generic;
+
+namespace SmartRestaurant.Domain.ValueObjects
+{
+    public class EnergeticValue : ValueObject 
+    {
+        public float Fat { get; set; }
+        public float Protein { get; set; }
+        public float Carbohydrates { get; set; }
+        public float Energy { get; set; }
+        public float Amount { get; set; }
+        public MeasurementUnits MeasurementUnit { get; set; }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            return new List<object>
+            {
+                Fat,
+                Protein,
+                Carbohydrates,
+                Energy,
+                Amount,
+                MeasurementUnit
+            };
+        }
+    }
+}
