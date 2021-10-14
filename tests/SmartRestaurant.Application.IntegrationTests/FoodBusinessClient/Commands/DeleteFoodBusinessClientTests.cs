b575d8268e7f4caa9edfdc35c958bd4768bce1ca
Khaoula Bouzekria <khaoulabouzekria@outlook.com>
2021-10-14 10:26:32 +0100

    -Adding delete FoodBusinessClient Tests

---
+++ tests/SmartRestaurant.Application.IntegrationTests/FoodBusinessClient/Commands/DeleteFoodBusinessClientTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.FoodBusinessClient.Commands;
+using SmartRestaurant.Application.IntegrationTests.TestTools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRestaurant.Application.IntegrationTests.FoodBusinessClient.Commands
+{
+    using static Testing;
+
+    [TestFixture]
+    public class DeleteFoodBusinessClientTests : TestBase
+    {
+        [Test]
+        public async Task DeleteFoodBusinessClientTask()
+        {
+            await RolesTestTools.CreateRoles();
+            var foodBusinessClientManager = await UsersTestTools.CreateFoodBusinessClientManager();
+            var foodBusinessClient = await FoodBusinessClientTestTools.CreateFoodBusinessClient(foodBusinessClientManager.Id);
+
+
+            await SendAsync(new DeleteFoodBusinessClientCommand
+            {
+                Id = foodBusinessClient.FoodBusinessClientId
+            });
+
+            var item = await FindAsync<Domain.Entities.FoodBusinessClient>(foodBusinessClient.FoodBusinessClientId);
+            item.Should().BeNull();
+        }
+    }
+}
---
+++ tests/SmartRestaurant.Application.Tests/CommandValidatorTests/FoodBusinessClient/DeleteFoodBusinessClientCommandValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation.TestHelper;
+using SmartRestaurant.Application.FoodBusinessClient.Commands;
+using System;
+using Xunit;
+
+namespace SmartRestaurant.Application.Tests.CommandValidatorTests.FoodBusinessClient
+{
+    public class DeleteFoodBusinessClientCommandValidatorTests
+    {
+        private readonly DeleteFoodBusinessClientCommandValidator _validator;
+
+        public DeleteFoodBusinessClientCommandValidatorTests()
+        {
+            _validator = new DeleteFoodBusinessClientCommandValidator();
+        }
+    
+        [Fact]
+        public void Given_IdIsEmptyGuid_WhenValidating_ShouldGetAnError()
+        {
+            var emptyGuid = Guid.Empty;
+            _validator.ShouldHaveValidationErrorFor(x => x.Id, emptyGuid);
+        }
+
+    }
+}
