d4bc9f92f92718b2c3099cc742a18d6e59cd44c2
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-10-18 04:34:29 +0200

    I have created a filter strategy for dish entity.

---
+++ src/Libraries/SmartRestaurant.Application/Dishes/Queries/FilterStrategy/IDishFilterStrategy.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartRestaurant.Application.Common.Extensions;
+using SmartRestaurant.Domain.Entities;
+
+namespace SmartRestaurant.Application.Dishes.Queries.FilterStrategy
+{
+    public interface IDishFilterStrategy
+    {     
+        public PagedResultBase<Dish> FetchData(DbSet<Dish> Dishes, GetDishesListQuery request);
+    }
+}
