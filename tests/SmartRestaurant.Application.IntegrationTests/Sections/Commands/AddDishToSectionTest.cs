c9fa123c2bff70c0f4092dc6255677bf9c0c5ab2
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-10-26 08:22:14 +0200

    [714] AffectMenuItemToSection : I have implemented integrations tests.

---
+++ tests/SmartRestaurant.Application.IntegrationTests/Sections/Commands/AddDishToSectionTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.IntegrationTests.TestTools;
+using SmartRestaurant.Application.Menus.Commands;
+using SmartRestaurant.Application.Sections.Commands;
+using SmartRestaurant.Domain.Entities;
+using SmartRestaurant.Domain.Enums;
+
+namespace SmartRestaurant.Application.IntegrationTests.Sections.Commands
+{
+    using static Testing;
+
+    [TestFixture]
+    public class AddDishToSectionTest : TestBase
+    {
+        [Test]
+        public async Task AddDishToSection_ShouldSaveToDB()
+        {
+            await RolesTestTools.CreateRoles();
+            var foodBusinessAdministrator = await UsersTestTools.CreateFoodBusinessAdministrator();
+            var fastFood = await FoodBusinessTestTools.CreateFoodBusiness(foodBusinessAdministrator.Id);
+            var createMenuCommand = await createMenu(fastFood);
+            var createSectionCommand = await createSection(createMenuCommand);
+            var createDishCommand = await createDish(fastFood);
+
+            var addDishToSectionCommand = new AddDishToSectionCommand
+            {
+                SectionId = createSectionCommand.Id.ToString(),
+                DishId = createDishCommand.Id.ToString(),
+            };
+            await SendAsync(addDishToSectionCommand);
+            var item = Where<SectionDish>(x => 
+            x.DishId == Guid.Parse(addDishToSectionCommand.DishId) && 
+            x.SectionId == Guid.Parse(addDishToSectionCommand.SectionId));
+
+            item.Should().NotBeNull();
+            item.Should().HaveCount(1);
+            item[0].SectionId.Should().Be(addDishToSectionCommand.SectionId);
+            item[0].DishId.Should().Be(addDishToSectionCommand.DishId);
+        }
+
+        private static async Task<Application.Dishes.Commands.CreateDishCommand> createDish(Domain.Entities.FoodBusiness fastFood)
+        {
+            var createIngredientCommand = await IngredientTestTools.CreateIngredient();
+            var createDishCommand = await DishTestTools.CreateDish(fastFood.FoodBusinessId, createIngredientCommand.Id);
+            return createDishCommand;
+        }
+
+        private static async Task<CreateSectionCommand> createSection(CreateMenuCommand createMenuCommand)
+        {
+            var createSectionCommand = new CreateSectionCommand
+            {
+                MenuId = createMenuCommand.Id,
+                Name = "section test menu"
+            };
+            await SendAsync(createSectionCommand);
+            return createSectionCommand;
+        }
+
+        private static async Task<CreateMenuCommand> createMenu(Domain.Entities.FoodBusiness fastFood)
+        {
+            var createMenuCommand = new CreateMenuCommand
+            {
+                Name = "test menu",
+                MenuState = (int)MenuState.Enabled,
+                FoodBusinessId = fastFood.FoodBusinessId
+            };
+            await SendAsync(createMenuCommand);
+            return createMenuCommand;
+        }
+    }
+}
---
+++ tests/SmartRestaurant.Application.IntegrationTests/Sections/Commands/AddProductToSectionTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.IntegrationTests.TestTools;
+using SmartRestaurant.Application.Menus.Commands;
+using SmartRestaurant.Application.Sections.Commands;
+using SmartRestaurant.Domain.Entities;
+using SmartRestaurant.Domain.Enums;
+
+namespace SmartRestaurant.Application.IntegrationTests.Sections.Commands
+{
+    using static Testing;
+
+    [TestFixture]
+    public class AddProductToSectionTest : TestBase
+    {
+        [Test]
+        public async Task AddProductToSection_ShouldSaveToDB()
+        {
+            await RolesTestTools.CreateRoles();
+            var foodBusinessAdministrator = await UsersTestTools.CreateFoodBusinessAdministrator();
+            var fastFood = await FoodBusinessTestTools.CreateFoodBusiness(foodBusinessAdministrator.Id);
+            var createMenuCommand = await createMenu(fastFood);
+            var createSectionCommand = await createSection(createMenuCommand);
+            var createProductCommand = await ProductTestTools.CreateProduct();
+
+            var addProductToSectionCommand = new AddProductToSectionCommand
+            {
+                SectionId = createSectionCommand.Id.ToString(),
+                ProductId = createProductCommand.Id.ToString(),
+            };
+            await SendAsync(addProductToSectionCommand);
+            var item = Where<SectionProduct>(x =>
+            x.ProductId == Guid.Parse(addProductToSectionCommand.ProductId) &&
+            x.SectionId == Guid.Parse(addProductToSectionCommand.SectionId));
+
+            item.Should().NotBeNull();
+            item.Should().HaveCount(1);
+            item[0].SectionId.Should().Be(addProductToSectionCommand.SectionId);
+            item[0].ProductId.Should().Be(addProductToSectionCommand.ProductId);
+        }
+
+        private static async Task<CreateSectionCommand> createSection(CreateMenuCommand createMenuCommand)
+        {
+            var createSectionCommand = new CreateSectionCommand
+            {
+                MenuId = createMenuCommand.Id,
+                Name = "section test menu"
+            };
+            await SendAsync(createSectionCommand);
+            return createSectionCommand;
+        }
+
+        private static async Task<CreateMenuCommand> createMenu(Domain.Entities.FoodBusiness fastFood)
+        {
+            var createMenuCommand = new CreateMenuCommand
+            {
+                Name = "test menu",
+                MenuState = (int)MenuState.Enabled,
+                FoodBusinessId = fastFood.FoodBusinessId
+            };
+            await SendAsync(createMenuCommand);
+            return createMenuCommand;
+        }
+    }
+}
