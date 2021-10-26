ba4a61f8346cf6b078b1f56c2cb7177bc754c492
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-10-26 21:17:32 +0200

    [725] GetSectionMenuItems : I have added a new endpoint to SectionsController that will be used to fetch section menu items and filter results by name.

--- src/Presentation/SmartRestaurant.API/Controllers/SectionsController.cs
+++ src/Presentation/SmartRestaurant.API/Controllers/SectionsController.cs
@@ -140,5 +140,33 @@ namespace SmartRestaurant.API.Controllers
             };
             return await SendWithErrorsHandlingAsync(command);
         }
+
+
+        /// <summary> GetSectionMenuItems() </summary>
+        /// <remarks>This endpoint allows <b>restaurant manager</b> to fetch list of menu items linked to section.<br></br><b>Note:</b>Menu items can be either a product or dish</remarks>
+        /// <param name="searchKey">Search keyword is used to filter results by <b>name</b></param>
+        /// <param name="id">id of section that we want to fetch its menu items.</param>
+        /// <param name="page">The start position of read pointer in a request results. Default value is: <b>1</b></param>
+        /// <param name="pageSize">The max number of results that should be returned. Default value is: <b>10</b>. Max value is: <b>100</b></param>
+        /// <response code="200"> Section menu items has been successfully fetched.<br></br><b>Note:</b> Picture will be encoded in Base64</response>
+        /// <response code="400">The payload data sent to the backend-server in order to fetch section menu items is invalid.</response>
+        /// <response code="401">The cause of 401 error is one of two reasons: Either the user is not logged into the application or authentication token is invalid or expired.</response>
+        /// <response code="403"> The user account you used to log into the application, does not have the necessary privileges to execute this request.</response>
+        [ProducesResponseType(typeof(PagedListDto<MenuItemDto>), 200)]
+        [ProducesResponseType(typeof(ExceptionResponse), 400)]
+        [Route("{id}/menu-items")]
+        [HttpGet]
+        [Authorize(Roles = "FoodBusinessManager,SupportAgent")]
+        public async Task<IActionResult> GetList([FromRoute] string id,  string searchKey, int page, int pageSize)
+        {
+            var query = new GetSectionMenuItemsQuery
+            {
+                SearchKey = searchKey,
+                Page = page,
+                PageSize = pageSize,
+                SectionId = id
+            };
+            return await SendWithErrorsHandlingAsync(query);
+        }
     }
 }
