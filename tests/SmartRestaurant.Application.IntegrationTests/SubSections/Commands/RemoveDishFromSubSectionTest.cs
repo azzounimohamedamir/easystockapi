52ad93687c34f890ae216dcca09652dabf220343
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-10-28 07:40:57 +0200

    [729] DeleteMenuItemFromSubSection : I have implemented integrations tests.

--- tests/SmartRestaurant.Application.IntegrationTests/SubSections/Commands/RemoveDishFromSubSectionTest.cs
+++ tests/SmartRestaurant.Application.IntegrationTests/SubSections/Commands/RemoveDishFromSubSectionTest.cs
@@ -5,54 +5,67 @@ using NUnit.Framework;
 using SmartRestaurant.Application.IntegrationTests.TestTools;
 using SmartRestaurant.Application.Menus.Commands;
 using SmartRestaurant.Application.Sections.Commands;
+using SmartRestaurant.Application.SubSections.Commands;
 using SmartRestaurant.Domain.Entities;
 using SmartRestaurant.Domain.Enums;
 
-namespace SmartRestaurant.Application.IntegrationTests.Sections.Commands
+namespace SmartRestaurant.Application.IntegrationTests.SubSections.Commands
 {
     using static Testing;
 
     [TestFixture]
-    public class RemoveDishFromSectionTest : TestBase
+    public class RemoveDishFromSubSectionTest : TestBase
     {
         [Test]
-        public async Task RemoveDishFromSection_ShouldRemovedFromDB()
+        public async Task RemoveDishFromSubSection_ShouldRemovedFromDB()
         {
             await RolesTestTools.CreateRoles();
             var foodBusinessAdministrator = await UsersTestTools.CreateFoodBusinessAdministrator();
             var fastFood = await FoodBusinessTestTools.CreateFoodBusiness(foodBusinessAdministrator.Id);
             var createMenuCommand = await CreateMenu(fastFood);
             var createSectionCommand = await CreateSection(createMenuCommand);
+            var createSubSectionCommand = await createSubSection(createSectionCommand);
             var createDishCommand = await CreateDish(fastFood);
 
-            var addDishToSectionCommand = await AddDishToSection(createSectionCommand, createDishCommand);
-            var item = Where<SectionDish>(x =>
-            x.DishId == Guid.Parse(addDishToSectionCommand.DishId) &&
-            x.SectionId == Guid.Parse(addDishToSectionCommand.SectionId));
+            var addDishToSubSectionCommand = await AddDishToSubSection(createSubSectionCommand, createDishCommand);
+            var item = Where<SubSectionDish>(x =>
+            x.DishId == Guid.Parse(addDishToSubSectionCommand.DishId) &&
+            x.SubSectionId == Guid.Parse(addDishToSubSectionCommand.SubSectionId));
             item.Should().NotBeNull();
             item.Should().HaveCount(1);
 
-            var removeDishFromSectionCommand = new RemoveDishFromSectionCommand
+            var removeDishFromSubSectionCommand = new RemoveDishFromSubSectionCommand
             {
-                SectionId = item[0].SectionId.ToString(),
+                SubSectionId = item[0].SubSectionId.ToString(),
                 DishId = item[0].DishId.ToString(),
             };
-            await SendAsync(removeDishFromSectionCommand);
-            var deletedItem = Where<SectionDish>(x =>
-                x.DishId == Guid.Parse(addDishToSectionCommand.DishId) &&
-                 x.SectionId == Guid.Parse(addDishToSectionCommand.SectionId));
+            await SendAsync(removeDishFromSubSectionCommand);
+            var deletedItem = Where<SubSectionDish>(x =>
+                x.DishId == Guid.Parse(addDishToSubSectionCommand.DishId) &&
+                x.SubSectionId == Guid.Parse(addDishToSubSectionCommand.SubSectionId));
             deletedItem.Should().HaveCount(0);
         }
 
-        private static async Task<AddDishToSectionCommand> AddDishToSection(CreateSectionCommand createSectionCommand, Application.Dishes.Commands.CreateDishCommand createDishCommand)
+        private static async Task<AddDishToSubSectionCommand> AddDishToSubSection(CreateSubSectionCommand createSubSectionCommand, Application.Dishes.Commands.CreateDishCommand createDishCommand)
         {
-            var addDishToSectionCommand = new AddDishToSectionCommand
+            var addDishToSubSectionCommand = new AddDishToSubSectionCommand
             {
-                SectionId = createSectionCommand.Id.ToString(),
+                SubSectionId = createSubSectionCommand.Id.ToString(),
                 DishId = createDishCommand.Id.ToString(),
             };
-            await SendAsync(addDishToSectionCommand);
-            return addDishToSectionCommand;
+            await SendAsync(addDishToSubSectionCommand);
+            return addDishToSubSectionCommand;
+        }
+
+        private static async Task<CreateSubSectionCommand> createSubSection(CreateSectionCommand createSectionCommand)
+        {
+            var createSubSectionCommand = new CreateSubSectionCommand
+            {
+                SectionId = createSectionCommand.Id,
+                Name = "sub section test menu"
+            };
+            await SendAsync(createSubSectionCommand);
+            return createSubSectionCommand;
         }
 
         private static async Task<Application.Dishes.Commands.CreateDishCommand> CreateDish(Domain.Entities.FoodBusiness fastFood)
--- tests/SmartRestaurant.Application.IntegrationTests/SubSections/Commands/RemoveProductFromSubSectionTest.cs
+++ tests/SmartRestaurant.Application.IntegrationTests/SubSections/Commands/RemoveProductFromSubSectionTest.cs
@@ -5,53 +5,67 @@ using NUnit.Framework;
 using SmartRestaurant.Application.IntegrationTests.TestTools;
 using SmartRestaurant.Application.Menus.Commands;
 using SmartRestaurant.Application.Sections.Commands;
+using SmartRestaurant.Application.SubSections.Commands;
 using SmartRestaurant.Domain.Entities;
 using SmartRestaurant.Domain.Enums;
 
-namespace SmartRestaurant.Application.IntegrationTests.Sections.Commands
+namespace SmartRestaurant.Application.IntegrationTests.SubSections.Commands
 {
     using static Testing;
 
     [TestFixture]
-    public class RemoveProductFromSectionTest : TestBase
+    public class RemoveProductFromSubSectionTest : TestBase
     {
         [Test]
-        public async Task RemoveProductFromSection_ShouldRemovedFromDB()
+        public async Task RemoveProductFromSubSection_ShouldRemovedFromDB()
         {
             await RolesTestTools.CreateRoles();
             var foodBusinessAdministrator = await UsersTestTools.CreateFoodBusinessAdministrator();
             var fastFood = await FoodBusinessTestTools.CreateFoodBusiness(foodBusinessAdministrator.Id);
             var createMenuCommand = await CreateMenu(fastFood);
             var createSectionCommand = await CreateSection(createMenuCommand);
+            var createSubSectionCommand = await createSubSection(createSectionCommand);
             var createProductCommand = await ProductTestTools.CreateProduct();
-            var addProductToSectionCommand = await AddProductToSection(createSectionCommand, createProductCommand);
-            var item = Where<SectionProduct>(x =>
-            x.ProductId == Guid.Parse(addProductToSectionCommand.ProductId) &&
-            x.SectionId == Guid.Parse(addProductToSectionCommand.SectionId));
+
+            var addProductToSubSectionCommand = await AddProductToSubSection(createSubSectionCommand, createProductCommand);
+            var item = Where<SubSectionProduct>(x =>
+            x.ProductId == Guid.Parse(addProductToSubSectionCommand.ProductId) &&
+            x.SubSectionId == Guid.Parse(addProductToSubSectionCommand.SubSectionId));
             item.Should().NotBeNull();
             item.Should().HaveCount(1);
 
-            var removeProductFromSectionCommand = new RemoveProductFromSectionCommand
+            var removeProductFromSubSectionCommand = new RemoveProductFromSubSectionCommand
             {
-                SectionId = item[0].SectionId.ToString(),
+                SubSectionId = item[0].SubSectionId.ToString(),
                 ProductId = item[0].ProductId.ToString(),
             };
-            await SendAsync(removeProductFromSectionCommand);
-            var deletedItem = Where<SectionProduct>(x =>
-                x.ProductId == Guid.Parse(addProductToSectionCommand.ProductId) &&
-                x.SectionId == Guid.Parse(addProductToSectionCommand.SectionId));
+            await SendAsync(removeProductFromSubSectionCommand);
+            var deletedItem = Where<SubSectionProduct>(x =>
+                x.ProductId == Guid.Parse(addProductToSubSectionCommand.ProductId) &&
+                x.SubSectionId == Guid.Parse(addProductToSubSectionCommand.SubSectionId));
             deletedItem.Should().HaveCount(0);
         }
 
-        private static async Task<AddProductToSectionCommand> AddProductToSection(CreateSectionCommand createSectionCommand, Application.Products.Commands.CreateProductCommand createProductCommand)
+        private static async Task<AddProductToSubSectionCommand> AddProductToSubSection(CreateSubSectionCommand createSubSectionCommand, Application.Products.Commands.CreateProductCommand createProductCommand)
         {
-            var addProductToSectionCommand = new AddProductToSectionCommand
+            var addProductToSubSectionCommand = new AddProductToSubSectionCommand
             {
-                SectionId = createSectionCommand.Id.ToString(),
+                SubSectionId = createSubSectionCommand.Id.ToString(),
                 ProductId = createProductCommand.Id.ToString(),
             };
-            await SendAsync(addProductToSectionCommand);
-            return addProductToSectionCommand;
+            await SendAsync(addProductToSubSectionCommand);
+            return addProductToSubSectionCommand;
+        }
+
+        private static async Task<CreateSubSectionCommand> createSubSection(CreateSectionCommand createSectionCommand)
+        {
+            var createSubSectionCommand = new CreateSubSectionCommand
+            {
+                SectionId = createSectionCommand.Id,
+                Name = "sub section test menu"
+            };
+            await SendAsync(createSubSectionCommand);
+            return createSubSectionCommand;
         }
 
         private static async Task<CreateSectionCommand> CreateSection(CreateMenuCommand createMenuCommand)
