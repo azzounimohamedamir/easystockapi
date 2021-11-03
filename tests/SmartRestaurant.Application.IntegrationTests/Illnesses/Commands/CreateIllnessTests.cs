eeebb3c14ce56fe12a37f326a182c66e69a647a5
Khaoula Bouekria <khaoulabouzekria@outlook.com>
2021-11-03 16:33:23 +0100

    -Add Create command tests

---
+++ tests/SmartRestaurant.Application.IntegrationTests/Illnesses/Commands/CreateIllnessTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.Illness.Commands;
+using SmartRestaurant.Application.IntegrationTests.TestTools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRestaurant.Application.IntegrationTests.Illnesses.Commands
+{
+    using static Testing;
+    [TestFixture]
+    public class CreateIllnessTests : TestBase
+    {
+        [Test]
+        public async Task CreateIllness_ShouldSaveToDB()
+        {
+            await RolesTestTools.CreateRoles();
+           
+            var createIllnessCommand = new CreateIllnessCommand
+            {
+                Name = "Illness test"
+            };
+            await SendAsync(createIllnessCommand);
+            var item = await FindAsync<Domain.Entities.Illness>(createIllnessCommand.Id);
+
+            item.Should().NotBeNull();
+            item.Name.Should().Be("Illness test");
+            item.IllnessId.Should().Be(createIllnessCommand.Id);
+        }
+    }
+}
---
+++ tests/SmartRestaurant.Application.Tests/MappingTests/IllnessMappingTest.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using SmartRestaurant.Application.Illness.Commands;
+using SmartRestaurant.Application.Tests.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SmartRestaurant.Application.Tests.MappingTests
+{
+    public class IllnessMappingTest : IClassFixture<MappingTestsFixture>
+    {
+        private readonly CreateIllnessCommandValidator _createIllnessValidator ;
+        private readonly IMapper _mapper;
+
+        public IllnessMappingTest(MappingTestsFixture fixture)
+        {
+            _mapper = fixture.Mapper;
+            _createIllnessValidator = new CreateIllnessCommandValidator();
+
+        }
+        [Fact]
+        public void Map_CreateIllnessCommand_To_Illness_Valide_Test()
+        {
+            var createIllnessCommand = new CreateIllnessCommand
+            {
+                Name = "Test"
+            };
+            var validationResult = _createIllnessValidator.Validate(createIllnessCommand);
+            Assert.True(validationResult.IsValid);
+            var illness = _mapper.Map<Domain.Entities.Illness>(createIllnessCommand);
+            Assert.Equal(illness.Name, createIllnessCommand.Name);
+            Assert.Equal(illness.IllnessId, createIllnessCommand.Id);
+        }
+    }
+}
