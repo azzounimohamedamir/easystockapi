e72e04908b09bd15e0b21079e03e5f0f053de227
Khaoula Bouekria <khaoulabouzekria@outlook.com>
2021-11-08 10:32:12 +0100

    Adding Get Illnesses List Query integration and unit tests

---
+++ tests/SmartRestaurant.Application.IntegrationTests/TestTools/IllnessTestTools.cs
@@ -0,0 +1,40 @@
+﻿using SmartRestaurant.Application.Common.Dtos;
+using SmartRestaurant.Application.Illness.Commands;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SmartRestaurant.Application.IntegrationTests.TestTools
+{
+    using static Testing;
+    public class IllnessTestTools
+    {
+        public static async Task<CreateIllnessCommand> CreateIllness(Guid ingredientId)
+        {
+            var createIllnessCommand = new CreateIllnessCommand
+            {
+                Name = "test",
+                Ingredients = new List<IngredientDto>()
+                {
+                    new IngredientDto()
+                    {
+                        IngredientId = ingredientId
+                    }
+                }
+            };
+            var createIllnessCommand2 = new CreateIllnessCommand
+            {
+                Name = "allergie",
+                Ingredients = new List<IngredientDto>()
+                {
+                    new IngredientDto()
+                    {
+                        IngredientId = ingredientId
+                    }
+                }
+            };
+            await SendAsync(createIllnessCommand2);
+            return createIllnessCommand;
+        }
+    }
+}
