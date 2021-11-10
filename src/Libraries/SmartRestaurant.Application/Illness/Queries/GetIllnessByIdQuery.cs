3acf3a72113ccfb7589b3feff9172aec0cd59e88
Khaoula Bouekria <khaoulabouzekria@outlook.com>
2021-11-10 07:13:40 +0100

    Adding Get illness by id query

---
+++ src/Libraries/SmartRestaurant.Application/Illness/Queries/GetIllnessByIdQuery.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+using SmartRestaurant.Application.Common.Dtos;
+using SmartRestaurant.Application.Common.Tools;
+using System;
+
+namespace SmartRestaurant.Application.Illness.Queries
+{
+    public class GetIllnessByIdQuery : IRequest<IllnessDto>
+    {
+        public string Id { get; set; }
+    }
+
+    public class GetIllnessByIdQueryValidator : AbstractValidator<GetIllnessByIdQuery>
+    {
+        public GetIllnessByIdQueryValidator()
+        {
+            RuleFor(illness => illness.Id)
+              .Cascade(CascadeMode.StopOnFirstFailure)
+              .NotEmpty()
+              .NotEqual(Guid.Empty.ToString())
+              .Must(ValidatorHelper.ValidateGuid).WithMessage("'{PropertyName}' must be a valid GUID");
+        }
+    }
+}
