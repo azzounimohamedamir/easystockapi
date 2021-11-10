29a3cfd7e6c2bf490986b780fb2d161755ae01be
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-11-10 08:22:11 +0100

    [738] List Orders : I have added a new endpoint that will be used to list and filter orders.

--- src/Presentation/SmartRestaurant.API/Controllers/OrdersController.cs
+++ src/Presentation/SmartRestaurant.API/Controllers/OrdersController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SmartRestaurant.API.Swagger.Exception;
+using SmartRestaurant.Application.Common.Dtos;
 using SmartRestaurant.Application.Common.Dtos.OrdersDtos;
+using SmartRestaurant.Application.Common.Enums;
 using SmartRestaurant.Application.Orders.Commands;
 using SmartRestaurant.Application.Orders.Queries;
 using Swashbuckle.AspNetCore.Annotations;
@@ -99,6 +101,42 @@ namespace SmartRestaurant.API.Controllers
         public async Task<IActionResult> Get([FromRoute] string id)
         {
             return await SendWithErrorsHandlingAsync(new GetOrderByIdQuery { Id = id });
+        }
+
+
+        /// <summary> GetListOfOrders() </summary>
+        /// <remarks>This endpoint allows us to fetch list of orders.</remarks>
+        /// <param name="currentFilter">Orders list can be filtred by: <b>order number</b></param>
+        /// <param name="searchKey">Search keyword</param>
+        /// <param name="sortOrder">Orders list can be sorted by: <b>acs</b> | <b>desc</b>. Default value is: <b>acs</b></param>
+        /// <param name="foodBusinessId">We will get dishes list linked to that foodBusinessId.</param>
+        /// <param name="dateInterval">We will get results within the selected interval. Default interval is: <b>ToDay</b><br></br>
+        ///     <b>Note 01:</b> This is the enum used to set Date Interval: <b>  enum DateFilter { ToDay, Last7Days, Last30Days, All } </b>
+        /// </param>
+        /// <param name="page">The start position of read pointer in a request results. Default value is: <b>1</b></param>
+        /// <param name="pageSize">The max number of Reservations that should be returned. Default value is: <b>10</b>. Max value is: <b>100</b></param>
+        /// <response code="200"> Orders list has been successfully fetched.</response>
+        /// <response code="400">The payload data sent to the backend-server in order to fetch orders list is invalid.</response>
+        /// <response code="401">The cause of 401 error is one of two reasons: Either the user is not logged into the application or authentication token is invalid or expired.</response>
+        /// <response code="403"> The user account you used to log into the application, does not have the necessary privileges to execute this request.</response>
+        [ProducesResponseType(typeof(PagedListDto<OrderDto>), 200)]
+        [ProducesResponseType(typeof(ExceptionResponse), 400)]
+        [Authorize(Roles = "FoodBusinessManager,Cashier")]
+        [HttpGet]
+        public Task<IActionResult> GetList(string currentFilter, string searchKey, string sortOrder, string foodBusinessId, 
+            int page, int pageSize,  DateFilter dateInterval)
+        {
+            var query = new GetOrdersListQuery
+            {
+                CurrentFilter = currentFilter,
+                SearchKey = searchKey,
+                SortOrder = sortOrder,
+                Page = page,
+                PageSize = pageSize,
+                FoodBusinessId = foodBusinessId,
+                DateInterval = dateInterval
+            };
+            return SendWithErrorsHandlingAsync(query);
         }
     }
 }
