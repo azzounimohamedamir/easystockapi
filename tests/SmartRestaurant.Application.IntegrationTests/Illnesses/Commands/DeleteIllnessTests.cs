a82daffa1a5811f2f70ab6774c9794fb796d371e
Khaoula Bouekria <khaoulabouzekria@outlook.com>
2021-11-10 16:25:18 +0100

    Adding delete illness unit and validation tests

---
+++ tests/SmartRestaurant.Application.IntegrationTests/Illnesses/Commands/DeleteIllnessTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.Illness.Commands;
+using SmartRestaurant.Application.IntegrationTests.TestTools;
+using System.Threading.Tasks;
+
+namespace SmartRestaurant.Application.IntegrationTests.Illnesses.Commands
+{
+    using static Testing;
+
+    [TestFixture]
+    class DeleteIllnessTests : TestBase
+    {
+        [Test]
+        public async Task DeleteIllness_ShouldBeRemovedFromDB()
+        {
+            var createIngredientCommand = await IngredientTestTools.CreateIngredient();
+            var createIllnessCommand = await IllnessTestTools.CreateIllness( createIngredientCommand.Id);
+            var checkIllnessExistance = await FindAsync<Domain.Entities.Illness>(createIllnessCommand.Id);
+            checkIllnessExistance.Should().NotBeNull();
+
+            await SendAsync(new DeleteIllnessCommand { Id = createIllnessCommand.Id.ToString() });
+            var deletedIllness = await FindAsync<Domain.Entities.Illness>(createIllnessCommand.Id);
+            deletedIllness.Should().BeNull();
+        }
+    }
+}
