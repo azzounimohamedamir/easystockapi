e4634b7610c3a1a62aa2160e0c38bbf123338d39
soufianeBenRahim <32791470+soufianeBenRahim@users.noreply.github.com>
2021-12-04 14:00:20 +0100

    add relation between zone and table entites

--- src/Libraries/SmartRestaurant.Domain/Entities/Zone.cs
+++ src/Libraries/SmartRestaurant.Domain/Entities/Zone.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using SmartRestaurant.Domain.Common;
 
 namespace SmartRestaurant.Domain.Entities
 {
     public class Zone : AuditableEntity
     {
+        public Zone()
+        {
+            Tables = new List<Table>();
+        }
         public Guid ZoneId { get; set; }
         public string ZoneTitle { get; set; }
         public Guid FoodBusinessId { get; set; }
         public FoodBusiness FoodBusiness { get; set; }
+        public virtual IList<Table> Tables { get; set; }
     }
 }
