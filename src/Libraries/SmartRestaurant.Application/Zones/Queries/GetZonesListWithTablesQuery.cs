7719a0478a7e8de0f6b020009dcb9829157ddc40
soufianeBenRahim <32791470+soufianeBenRahim@users.noreply.github.com>
2021-12-05 16:16:29 +0100

    add validator to  GetZonesListWithTablesQuery

--- src/Libraries/SmartRestaurant.Application/Zones/Queries/GetZonesListWithTablesQuery.cs
+++ src/Libraries/SmartRestaurant.Application/Zones/Queries/GetZonesListWithTablesQuery.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using FluentValidation;
 using MediatR;
 using SmartRestaurant.Application.Common.Dtos;
+using SmartRestaurant.Application.Common.Tools;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
 
 namespace SmartRestaurant.Application.Zones.Queries
 {
     public class GetZonesListWithTablesQuery : IRequest<IEnumerable<ZoneWithTablesDto>>
     {
-        public Guid FoodBusinessId { get; set; }
+        [SwaggerSchema(ReadOnly = true)] public string FoodBusinessId { get; set; }
+    }
+    public class GetZonesListWithTablesQueryValidator : AbstractValidator<GetZonesListWithTablesQuery>
+    {
+        public GetZonesListWithTablesQueryValidator()
+        {
+            RuleFor(foodBusiness => foodBusiness.FoodBusinessId)
+              .Cascade(CascadeMode.StopOnFirstFailure)
+              .NotEmpty()
+              .NotEqual(Guid.Empty.ToString())
+              .Must(ValidatorHelper.ValidateGuid).WithMessage("'{PropertyName}' must be a valid GUID");
+        }
     }
 }
