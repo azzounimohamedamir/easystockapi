2c6fb9474d02d65eabf908b3c1158f9ce08e979e
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-12-07 10:43:34 +0100

    [785] FoodbusinessActiveMenu : I have implemented integrations tests.

---
+++ tests/SmartRestaurant.Application.IntegrationTests/Menus/Queries/GetActiveMenuTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.IntegrationTests.TestTools;
+using SmartRestaurant.Application.Menus.Commands;
+using SmartRestaurant.Application.Menus.Queries;
+using SmartRestaurant.Domain.Enums;
+
+namespace SmartRestaurant.Application.IntegrationTests.Menus.Queries
+{
+    using static Testing;
+
+    [TestFixture]
+    public class GetActiveMenuTest : TestBase
+    {
+        [Test]
+        public async Task ShouldGetAllMenus_ByFoodBusinessId()
+        {
+            await RolesTestTools.CreateRoles();
+            var foodBusinessAdministrator = await UsersTestTools.CreateFoodBusinessAdministrator();
+            var fastFood = await FoodBusinessTestTools.CreateFoodBusiness(foodBusinessAdministrator.Id);
+
+            for (var i = 0; i < 5; i++)
+            {
+                await SendAsync(new CreateMenuCommand
+                {
+                    Name = "tacos Dz  " + i,
+                    FoodBusinessId = fastFood.FoodBusinessId
+                });
+            }
+
+            var menuId = Guid.NewGuid();
+            await SendAsync(new CreateMenuCommand
+            {
+                Id = menuId,
+                Name = "test menu",
+                FoodBusinessId = fastFood.FoodBusinessId
+            });
+            await SendAsync(new UpdateMenuCommand
+            {
+                Id = menuId,
+                Name = "test menu -- enabled",
+                MenuState = MenuState.Enabled,
+                FoodBusinessId = fastFood.FoodBusinessId
+            });
+
+            var result = await SendAsync(new GetActiveMenuQuery { FoodBusinessId = fastFood.FoodBusinessId.ToString() });
+            result.MenuId.Should().Be(menuId);
+            result.Name.Should().Be("test menu -- enabled");
+        }
+    }
+}
