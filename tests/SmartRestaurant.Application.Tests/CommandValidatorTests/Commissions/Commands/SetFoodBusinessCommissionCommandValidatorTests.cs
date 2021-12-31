5d218384a1aac8eb750e237b6b69cd464c919e45
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2021-12-31 01:49:20 +0100

    [814] SRUserSetFoodBusinessComission : I have implemented unit tests.

---
+++ tests/SmartRestaurant.Application.Tests/CommandValidatorTests/Commissions/Commands/SetFoodBusinessCommissionCommandValidatorTests.cs
@@ -0,0 +1,90 @@
+﻿using FluentValidation.TestHelper;
+using SmartRestaurant.Application.commisiones.Commands;
+using SmartRestaurant.Domain.Enums;
+using System;
+using Xunit;
+
+namespace SmartRestaurant.Application.Tests.CommandValidatorTests.Commissions.Commands
+{
+    public class SetFoodBusinessCommissionCommandValidatorTests
+    {
+        private readonly SetFoodBusinessCommissionCommandValidator _validator;
+
+        public SetFoodBusinessCommissionCommandValidatorTests()
+        {
+            _validator = new SetFoodBusinessCommissionCommandValidator();
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIdIsEmpty_WhenValidating_ShouldGetAnError()
+        {
+            var empty = "";
+            _validator.ShouldHaveValidationErrorFor(x => x.FoodBusinessId, empty);
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIdIsEmptyGuid_WhenValidating_ShouldGetAnError()
+        {
+            var emptyGuid = Guid.Empty.ToString();
+            _validator.ShouldHaveValidationErrorFor(x => x.FoodBusinessId, emptyGuid);
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIdIsInvalidGuid_WhenValidating_ShouldGetAnError()
+        {
+            var invalidGuid = "3cbf3570-4444-4673-8746-2";
+            _validator.ShouldHaveValidationErrorFor(x => x.FoodBusinessId, invalidGuid);
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIdIsValid_WhenValidating_ShouldBeValidated()
+        {
+            var validEntry = "acbf657b-3398-7a73-8746-77b7cf596315";
+            _validator.ShouldNotHaveValidationErrorFor(x => x.FoodBusinessId, validEntry);
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIdIsNull_WhenValidating_ShouldGetAnError()
+        {
+            string _null = null;
+            _validator.ShouldHaveValidationErrorFor(x => x.FoodBusinessId, _null);
+        }
+
+        [Fact]
+        public void Given_ValueIsLessThanZero_WhenValidating_ShouldGetAnError()
+        {
+            _validator.ShouldHaveValidationErrorFor(x => x.Value, -1);
+        }
+
+        [Fact]
+        public void Given_ValueIsValid_WhenValidating_ShouldBeValidated()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(x => x.Value, 0);
+        }
+
+        [Fact]
+        public void Given_TypePerPersonIsValid_WhenValidating_ShouldBeValidated()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(x => x.Type, CommissionType.PerPerson);
+        }
+
+        [Fact]
+        public void Given_TypePerOrderIsValid_WhenValidating_ShouldBeValidated()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(x => x.Type, CommissionType.PerOrder);
+        }
+
+        [Fact]
+        public void Given_WhoPayFoodBusinessIsValid_WhenValidating_ShouldBeValidated()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(x => x.WhoPay, WhoPayCommission.FoodBusiness);
+        }
+
+        [Fact]
+        public void Given_WhoPayFoodBusinessCustomerIsValid_WhenValidating_ShouldBeValidated()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(x => x.WhoPay, WhoPayCommission.FoodBusinessCustomer);
+        }
+
+    }
+}
---
+++ tests/SmartRestaurant.Application.Tests/MappingTests/CommissionMappingTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using AutoMapper;
+using SmartRestaurant.Application.commisiones.Commands;
+using SmartRestaurant.Application.Tests.Configuration;
+using SmartRestaurant.Domain.Enums;
+using SmartRestaurant.Domain.ValueObjects;
+using Xunit;
+
+namespace SmartRestaurant.Application.Tests.MappingTests
+{
+    public class CommissionMappingTest : IClassFixture<MappingTestsFixture>
+    {
+        private readonly IMapper _mapper;
+
+        public CommissionMappingTest(MappingTestsFixture fixture)
+        {
+            _mapper = fixture.Mapper;
+        }
+      
+
+        [Fact]
+        public void Map_SetFoodBusinessCommissionCommand_To_FoodBusiness_Valide_Test()
+        {
+            var setFoodBusinessCommissionCommand = new SetFoodBusinessCommissionCommand
+            {
+                Value = 25,
+                Type = CommissionType.PerPerson,
+                WhoPay = WhoPayCommission.FoodBusinessCustomer,
+            };
+
+            var foodBusiness = new Domain.Entities.FoodBusiness
+            {
+                AcceptsCreditCards = true,
+                AcceptTakeout = true,
+                Address = new Address
+                {
+                    City = "Algiers",
+                    Country = "Algeria",
+                    GeoPosition = new GeoPosition
+                    {
+                        Latitude = "0",
+                        Longitude = "0"
+                    },
+                    StreetAddress = "Didouche Mourad"
+                },
+                Description = "",
+                HasCarParking = true,
+                IsHandicapFriendly = true,
+                Name = "Taj mahal",
+                OffersTakeout = true,
+                PhoneNumber = new PhoneNumber { CountryCode = 213, Number = 670217536 },
+                Tags = "pizza;sandwish",
+                Email = "test@g22.com",
+                Website = "",
+                FoodBusinessAdministratorId = Guid.NewGuid().ToString(),
+                FoodBusinessCategory = FoodBusinessCategory.Restaurant,
+                DefaultCurrency = Currencies.USD,
+                CommissionConfigs = null
+            };
+
+
+
+            _mapper.Map(setFoodBusinessCommissionCommand, foodBusiness);
+            Assert.Equal(foodBusiness.AcceptsCreditCards, foodBusiness.AcceptsCreditCards);
+            Assert.Equal(foodBusiness.AcceptTakeout, foodBusiness.AcceptTakeout);
+            Assert.Equal(foodBusiness.Address.City, foodBusiness.Address.City);
+            Assert.Equal(foodBusiness.Address.StreetAddress, foodBusiness.Address.StreetAddress);
+            Assert.Equal(foodBusiness.Address.Country, foodBusiness.Address.Country);
+            Assert.Equal(foodBusiness.Address.GeoPosition.Latitude, foodBusiness.Address.GeoPosition.Latitude);
+            Assert.Equal(foodBusiness.Address.GeoPosition.Longitude, foodBusiness.Address.GeoPosition.Longitude);
+            Assert.Equal(foodBusiness.Description, foodBusiness.Description);
+            Assert.Equal(foodBusiness.HasCarParking, foodBusiness.HasCarParking);
+            Assert.Equal(foodBusiness.IsHandicapFriendly, foodBusiness.IsHandicapFriendly);
+            Assert.Equal(foodBusiness.Name, foodBusiness.Name);
+            Assert.Equal(foodBusiness.OffersTakeout, foodBusiness.OffersTakeout);
+            Assert.Equal(foodBusiness.PhoneNumber.CountryCode, foodBusiness.PhoneNumber.CountryCode);
+            Assert.Equal(foodBusiness.PhoneNumber.Number, foodBusiness.PhoneNumber.Number);
+            Assert.Equal(foodBusiness.Tags, foodBusiness.Tags);
+            Assert.Equal(foodBusiness.Email, foodBusiness.Email);
+            Assert.Equal(foodBusiness.Website, foodBusiness.Website);
+            Assert.Equal(foodBusiness.FoodBusinessAdministratorId, foodBusiness.FoodBusinessAdministratorId);
+            Assert.Equal(foodBusiness.DefaultCurrency, foodBusiness.DefaultCurrency);
+            Assert.False(foodBusiness.IsActivityFrozen);
+            Assert.Equal(setFoodBusinessCommissionCommand.Value, foodBusiness.CommissionConfigs.Value);
+            Assert.Equal(setFoodBusinessCommissionCommand.Type, foodBusiness.CommissionConfigs.Type);
+            Assert.Equal(setFoodBusinessCommissionCommand.WhoPay, foodBusiness.CommissionConfigs.WhoPay);
+
+
+
+
+
+        }
+
+    }
+}
