91e32764ac7e04770fed82da35df110e69a9390f
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-04 12:02:10 +0100

    [816] GenerateMonthlyCommission : have created a task scheduler that will triger the task used to [calculate last month commission fees for all foodbusinesses] each month.

---
+++ src/Presentation/SmartRestaurant.API/Scheduler/MonthlyCommissionScheduler.cs
@@ -0,0 +1,31 @@
+﻿using SmartRestaurant.Application.commisiones.Commands;
+using SmartRestaurant.Application.Services;
+using System;
+using System.Diagnostics;
+
+namespace SmartRestaurant.API.Scheduler
+{
+
+    public class CommissionScheduler
+    {
+        public void ExecuteCommissionMonthlyTasks()
+        {
+            var hour = 6;
+            var minute = 0;
+
+            ApplicationScheduler.IntervalInHours(hour, minute, 24, MonthlyCommission());
+            Action MonthlyCommission()
+            {
+                return async () =>
+                {
+                    DateTime now = DateTime.Now;
+                    if(now.Day == 1)
+                    {
+                        Debug.WriteLine("########## Calculate Last Month Commission Fees ##########");
+                        await new MediatorService().SendAsync(new CalculateLastMonthCommissionFeesCommand());
+                    }
+                };
+            }
+        }
+    }
+}
