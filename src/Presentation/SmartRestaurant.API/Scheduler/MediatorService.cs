348addb2d9fba64bb9ecbea888c8a9c7239d60bc
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-04 11:55:07 +0100

    [816] GenerateMonthlyCommission : I have created MediatorService to call API Handlers outside the Controllers .

---
+++ src/Presentation/SmartRestaurant.API/Scheduler/MediatorService.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System.Threading.Tasks;
+
+namespace SmartRestaurant.API.Scheduler
+{
+    public class MediatorService
+    {
+        private static IMediator _mediator;
+
+        public MediatorService(IMediator mediator)
+        {
+            if(_mediator == null)
+            {
+                _mediator = mediator;
+            }
+        }
+
+        public MediatorService()
+        {           
+        }
+
+        public IMediator Mediator()
+        {
+            return _mediator;
+        }
+
+        public async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        {
+            return await _mediator.Send(request).ConfigureAwait(false);
+        }
+    }
+}
