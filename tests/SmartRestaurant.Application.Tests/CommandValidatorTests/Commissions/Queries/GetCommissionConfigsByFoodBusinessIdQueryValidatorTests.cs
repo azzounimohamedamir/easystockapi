a55131a9ea04e9c0ecc306d84e636871b8e0110f
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-08 18:37:44 +0100

    [830] User Display Subscription : I have implemented unit tests.

---
+++ tests/SmartRestaurant.Application.Tests/CommandValidatorTests/Commissions/Queries/GetCommissionConfigsByFoodBusinessIdQueryValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation.TestHelper;
+using SmartRestaurant.Application.commisiones.Queries;
+using System;
+using Xunit;
+
+namespace SmartRestaurant.Application.Tests.CommandValidatorTests.Commissions.Queries
+{
+    public class GetCommissionConfigsByFoodBusinessIdQueryValidatorTests
+    {
+        private readonly GetCommissionConfigsByFoodBusinessIdQueryValidator _validator;
+
+        public GetCommissionConfigsByFoodBusinessIdQueryValidatorTests()
+        {
+            _validator = new GetCommissionConfigsByFoodBusinessIdQueryValidator();
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIdIsEmpty_WhenValidating_ShouldGetAnError()
+        {
+            var empty = "";
+            _validator.ShouldHaveValidationErrorFor(x => x.FoodBusinessId, empty);
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIdIsEmptyGuid_WhenValidating_ShouldGetAnError()
+        {
+            var emptyGuid = Guid.Empty.ToString();
+            _validator.ShouldHaveValidationErrorFor(x => x.FoodBusinessId, emptyGuid);
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIdIsInvalidGuid_WhenValidating_ShouldGetAnError()
+        {
+            var invalidGuid = "3cbf3570-4444-4673-8746-2";
+            _validator.ShouldHaveValidationErrorFor(x => x.FoodBusinessId, invalidGuid);
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIdIsValid_WhenValidating_ShouldBeValidated()
+        {
+            var validEntry = "acbf657b-3398-7a73-8746-77b7cf596315";
+            _validator.ShouldNotHaveValidationErrorFor(x => x.FoodBusinessId, validEntry);
+        }
+
+        [Fact]
+        public void Given_FoodBusinessIdIsNull_WhenValidating_ShouldGetAnError()
+        {
+            string _null = null;
+            _validator.ShouldHaveValidationErrorFor(x => x.FoodBusinessId, _null);
+        }     
+    }
+}
