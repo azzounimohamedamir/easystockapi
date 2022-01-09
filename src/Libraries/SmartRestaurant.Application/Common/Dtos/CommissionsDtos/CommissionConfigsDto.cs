45e7288b0d48af1e9a80acd0645cf6975d7624b4
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-09 05:00:56 +0100

    [818] SR-UserListComissionConfigs : I have added FoodBusinessName to CommissionConfigsDto.

--- src/Libraries/SmartRestaurant.Application/Common/Dtos/CommissionsDtos/CommissionConfigsDto.cs
+++ src/Libraries/SmartRestaurant.Application/Common/Dtos/CommissionsDtos/CommissionConfigsDto.cs
@@ -5,6 +5,7 @@ namespace SmartRestaurant.Application.Common.Dtos.CommissionsDtos
 {
     public class CommissionConfigsDto
     {
+        public string FoodBusinessName { get; set; }
         public Guid FoodBusinessId { get; set; }
         public float Value { get; set; }
         public CommissionType Type { get; set; }
