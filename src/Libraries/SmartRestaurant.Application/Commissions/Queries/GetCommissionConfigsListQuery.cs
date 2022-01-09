e127c9f5f35da82fa845f13c953a285721cd5ca1
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-09 05:04:06 +0100

    [818] SR-UserListComissionConfigs : I have created a handler that will be used to fetch the list of commissions configs.

---
+++ src/Libraries/SmartRestaurant.Application/Commissions/Queries/GetCommissionConfigsListQuery.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+using SmartRestaurant.Application.Common.Dtos;
+using SmartRestaurant.Application.Common.Dtos.CommissionsDtos;
+
+namespace SmartRestaurant.Application.commisiones.Queries
+{
+    public class GetCommissionConfigsListQuery : IRequest<PagedListDto<CommissionConfigsDto>>
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public string SearchKey { get; set; }
+        public string SortOrder { get; set; }
+        public string CurrentFilter { get; set; }
+    }
+
+    public class GetCommissionConfigsListQueryValidator : AbstractValidator<GetCommissionConfigsListQuery>
+    {
+        public GetCommissionConfigsListQueryValidator()
+        {
+            RuleFor(v => v.PageSize)              
+                .LessThanOrEqualTo(100);
+        }
+    }
+}
