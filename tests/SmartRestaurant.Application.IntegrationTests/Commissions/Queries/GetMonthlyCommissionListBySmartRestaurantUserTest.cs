4fb6c0a298897fb96792910344a54989ef40e5d0
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-16 12:55:43 +0100

    [812] SR-UserListMonthlyCommissions : I have implemented Integration tests.

---
+++ tests/SmartRestaurant.Application.IntegrationTests/Commissions/Queries/GetMonthlyCommissionListBySmartRestaurantUserTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.commisiones.Commands;
+using SmartRestaurant.Application.CommissionsMonthlyFees.Commands;
+using SmartRestaurant.Application.CommissionsMonthlyFees.Queries;
+using SmartRestaurant.Application.IntegrationTests.TestTools;
+using SmartRestaurant.Domain.Enums;
+
+namespace SmartRestaurant.Application.IntegrationTests.Commissions.Queries
+{
+    using static Testing;
+
+    public class GetMonthlyCommissionListBySmartRestaurantUserTest : TestBase
+    {
+        [Test]
+        public async Task ShouldReturnMonthlyCommissionListBySmartRestauranUser()
+        {
+            await RolesTestTools.CreateRoles();
+            var foodBusinessAdministrator = await UsersTestTools.CreateFoodBusinessAdministrator(_authenticatedUserId);
+            var fastFood = await FoodBusinessTestTools.CreateFoodBusiness(foodBusinessAdministrator.Id);
+            var setFoodBusinessCommissionCommand = new SetFoodBusinessCommissionCommand
+                {
+                    FoodBusinessId = fastFood.FoodBusinessId.ToString(),
+                    Value = 25,
+                    Type = CommissionType.PerPerson,
+                    WhoPay = WhoPayCommission.FoodBusinessCustomer,
+                };
+                await SendAsync(setFoodBusinessCommissionCommand);
+                await SendAsync(new CalculateLastMonthCommissionFeesCommand());
+
+            var result00 = await SendAsync(new GetMonthlyCommissionListBySmartRestaurantUserQuery
+            {
+                Page = 1,
+                PageSize = 5,
+                CurrentFilter = "foodbusinessname",
+                SearchKey = "Taj mahal"
+            });
+            result00.Data.Should().HaveCount(1);
+
+
+            var result01 = await SendAsync(new GetMonthlyCommissionListBySmartRestaurantUserQuery
+            {
+                Page = 1,
+                PageSize = 5,
+                CurrentFilter = "foodbusinessname",
+                SearchKey = "Taj mahal m"
+            });
+            result01.Data.Should().HaveCount(0);
+
+
+            var result02 = await SendAsync(new GetMonthlyCommissionListBySmartRestaurantUserQuery { 
+                Page = 1, 
+                PageSize = 5, 
+                CurrentFilter = "foodbusinessadmin",
+                SearchKey = "FoodBusinessAdministrator@bv.com" });
+            result02.Data.Should().HaveCount(1);
+
+
+            var result03 = await SendAsync(new GetMonthlyCommissionListBySmartRestaurantUserQuery { 
+                Page = 1, 
+                PageSize = 5,
+                CurrentFilter = "foodbusinessadmin",
+                SearchKey = "test@bv.com" });
+            result03.Data.Should().HaveCount(0);
+
+
+            var result04 = await SendAsync(new GetMonthlyCommissionListBySmartRestaurantUserQuery
+            {
+                Page = 1,
+                PageSize = 5
+            });
+            result04.Data.Should().HaveCount(1);
+        }
+    }
+}
