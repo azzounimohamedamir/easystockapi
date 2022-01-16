a0b89709825cdb515c71cb0a47205963f9d995f1
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-16 12:30:05 +0100

    [812] SR-UserListMonthlyCommissions : I have created a new endpoint that will be used to fetch monthly commissions for food businesses.

--- src/Presentation/SmartRestaurant.API/Controllers/CommissionsMonthlyFeesController.cs
+++ src/Presentation/SmartRestaurant.API/Controllers/CommissionsMonthlyFeesController.cs
@@ -41,6 +41,34 @@ namespace SmartRestaurant.API.Controllers
         }
 
 
+        /// <summary> GetMonthlyCommissionListBySmartRestaurantUser() </summary>
+        /// <remarks>This endpoint allows SmartRestaurant User to fetch list of monthly commissions for the selected FoodBusiness.</remarks>
+        /// <param name="currentFilter">List of monthly commission can be filtred by: <b>foodbusinessname</b> | <b>foodbusinessadmin</b>. Default value is: <b>foodbusinessname</b></param>
+        /// <param name="searchKey">Search keyword</param>        
+        /// <param name="page">The start position of read pointer in a request results. Default value is: <b>1</b></param>
+        /// <param name="pageSize">The max number of Reservations that should be returned. Default value is: <b>10</b>. Max value is: <b>100</b></param>
+        /// <response code="200"> List of monthly commission has been successfully fetched.</response>
+        /// <response code="400">The payload data sent to the backend-server in order to fetch list of monthly commission is invalid.</response>
+        /// <response code="401">The cause of 401 error is one of two reasons: Either the user is not logged into the application or authentication token is invalid or expired.</response>
+        /// <response code="403"> The user account you used to log into the application, does not have the necessary privileges to execute this request.</response>
+        [ProducesResponseType(typeof(PagedListDto<CommissionConfigsDto>), 200)]
+        [ProducesResponseType(typeof(ExceptionResponse), 400)]
+        [Authorize(Roles = "SuperAdmin,SupportAgent,SalesMan")]
+        [Route("monthly-commissions-by-smartrestaurant-user")]
+        [HttpGet]
+        public Task<IActionResult> GetMonthlyCommissionListBySmartRestaurantUser(string searchKey, string currentFilter, int page, int pageSize)
+        {
+            var query = new GetMonthlyCommissionListBySmartRestaurantUserQuery
+            {
+                SearchKey = searchKey,
+                CurrentFilter = currentFilter,
+                Page = page,
+                PageSize = pageSize
+            };
+            return SendWithErrorsHandlingAsync(query);
+        }
+
+
         /// <summary> CalculateLastMonthCommissionFees() </summary>
         /// <remarks>This endpoint allows user to manually Calculate Last Month Commission Fees for all foodbusinesses in database. </remarks>        
         /// <response code="204">Calculation last month commission fees has been successfully done.</response>
