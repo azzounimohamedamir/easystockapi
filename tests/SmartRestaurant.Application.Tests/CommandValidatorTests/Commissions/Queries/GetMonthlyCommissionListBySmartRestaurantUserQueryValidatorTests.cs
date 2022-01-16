d27fb3e2c6cd0ebc693c61b9e74ce74fffdd9dc8
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-16 12:40:56 +0100

    [812] SR-UserListMonthlyCommissions : I have implemented unit tests.

---
+++ tests/SmartRestaurant.Application.Tests/CommandValidatorTests/Commissions/Queries/GetMonthlyCommissionListBySmartRestaurantUserQueryValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.TestHelper;
+using SmartRestaurant.Application.CommissionsMonthlyFees.Queries;
+using System;
+using Xunit;
+
+namespace SmartRestaurant.Application.Tests.CommandValidatorTests.Commissions.Queries
+{
+    public class GetMonthlyCommissionListBySmartRestaurantUserQueryValidatorTests
+    {
+        private readonly GetMonthlyCommissionListBySmartRestaurantUserQueryValidator _validator;
+
+        public GetMonthlyCommissionListBySmartRestaurantUserQueryValidatorTests()
+        {
+            _validator = new GetMonthlyCommissionListBySmartRestaurantUserQueryValidator();
+        }
+
+        [Fact]
+        public void Given_PageSizeExceedMax_WhenValidating_ShouldGetAnError()
+        {
+            int exceedMax = 101;
+            _validator.ShouldHaveValidationErrorFor(x => x.PageSize, exceedMax);
+        }
+    }
+}
