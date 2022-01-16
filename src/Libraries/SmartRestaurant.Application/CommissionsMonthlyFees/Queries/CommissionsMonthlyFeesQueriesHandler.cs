3e3bb82dcd01b4d079aa3ea03f0b603904e58f0e
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-16 12:23:49 +0100

    [812] SR-UserListMonthlyCommissions : I have created a handler that will be used to fetch monthly commissions for food businesses.

--- src/Libraries/SmartRestaurant.Application/CommissionsMonthlyFees/Queries/CommissionsMonthlyFeesQueriesHandler.cs
+++ src/Libraries/SmartRestaurant.Application/CommissionsMonthlyFees/Queries/CommissionsMonthlyFeesQueriesHandler.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using SmartRestaurant.Application.CommissionsMonthlyFees.Queries.FilterStrategy;
 using SmartRestaurant.Application.Common.Dtos;
 using SmartRestaurant.Application.Common.Dtos.CommissionsDtos;
 using SmartRestaurant.Application.Common.Exceptions;
@@ -14,23 +15,27 @@ using SmartRestaurant.Application.Common.Interfaces;
 
 namespace SmartRestaurant.Application.CommissionsMonthlyFees.Queries
 {
-    public class CommissionsMonthlyFeesQueriesHandler : 
-        IRequestHandler<GetMonthlyCommissionListByFoodBusinessUserQuery, PagedListDto<MonthlyCommissionDto>>
+    public class CommissionsMonthlyFeesQueriesHandler :
+        IRequestHandler<GetMonthlyCommissionListByFoodBusinessUserQuery, PagedListDto<MonthlyCommissionDto>>,
+        IRequestHandler<GetMonthlyCommissionListBySmartRestaurantUserQuery, PagedListDto<MonthlyCommissionDto>>
+
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IIdentityContext _identityContext;
 
-        public CommissionsMonthlyFeesQueriesHandler(IApplicationDbContext context, IMapper mapper)
+        public CommissionsMonthlyFeesQueriesHandler(IApplicationDbContext context, IMapper mapper, IIdentityContext identityContext)
         {
             _context = context;
             _mapper = mapper;
+            _identityContext = identityContext;
         }
 
         public async Task<PagedListDto<MonthlyCommissionDto>> Handle(GetMonthlyCommissionListByFoodBusinessUserQuery request, CancellationToken cancellationToken)
         {
             var validator = new GetMonthlyCommissionListByFoodBusinessUserQueryValidator();
             var result = await validator.ValidateAsync(request, cancellationToken).ConfigureAwait(false);
-            if (!result.IsValid) 
+            if (!result.IsValid)
                 throw new ValidationException(result);
 
             var query =  _context.MonthlyCommission
@@ -39,6 +44,22 @@ namespace SmartRestaurant.Application.CommissionsMonthlyFees.Queries
                 .OrderBy(x => x.Status)
                 .ThenByDescending(x => x.Month)
                 .GetPaged(request.Page, request.PageSize);
+
+            var queryData = await query.Data.ToListAsync(cancellationToken).ConfigureAwait(false);
+            var data = _mapper.Map<List<MonthlyCommissionDto>>(queryData);
+
+            return new PagedListDto<MonthlyCommissionDto>(query.CurrentPage, query.PageCount, query.PageSize, query.RowCount, data);
+        }
+
+        public async Task<PagedListDto<MonthlyCommissionDto>> Handle(GetMonthlyCommissionListBySmartRestaurantUserQuery request, CancellationToken cancellationToken)
+        {
+            var validator = new GetMonthlyCommissionListBySmartRestaurantUserQueryValidator();
+            var result = await validator.ValidateAsync(request, cancellationToken).ConfigureAwait(false);
+            if (!result.IsValid)
+                throw new ValidationException(result);
+
+            var filter = CommissionsMonthlyFeesStrategies.GetFilterStrategy(request.CurrentFilter);
+            var query = filter.FetchData(_context, _identityContext, request);
 
             var queryData = await query.Data.ToListAsync(cancellationToken).ConfigureAwait(false);
             var data = _mapper.Map<List<MonthlyCommissionDto>>(queryData);
---
+++ src/Libraries/SmartRestaurant.Application/CommissionsMonthlyFees/Queries/GetMonthlyCommissionListBySmartRestaurantUserQuery.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MediatR;
+using SmartRestaurant.Application.Common.Dtos;
+using SmartRestaurant.Application.Common.Dtos.CommissionsDtos;
+
+namespace SmartRestaurant.Application.CommissionsMonthlyFees.Queries
+{
+    public class GetMonthlyCommissionListBySmartRestaurantUserQuery : IRequest<PagedListDto<MonthlyCommissionDto>>
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public string SearchKey { get; set; }
+        public string CurrentFilter { get; set; }
+    }
+
+    public class GetMonthlyCommissionListBySmartRestaurantUserQueryValidator : AbstractValidator<GetMonthlyCommissionListBySmartRestaurantUserQuery>
+    {
+        public GetMonthlyCommissionListBySmartRestaurantUserQueryValidator()
+        {
+            RuleFor(x => x.PageSize)              
+                .LessThanOrEqualTo(100);
+
+            RuleFor(x => x.SearchKey)
+                .MaximumLength(200);
+        }
+    }
+}
