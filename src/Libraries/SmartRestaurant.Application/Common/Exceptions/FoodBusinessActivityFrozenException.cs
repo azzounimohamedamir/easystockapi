3826abfa204cb6b0c29cbdb497893454427043b0
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-17 03:40:08 +0100

    [833] AutomaticallyFreezeFoodBusinessActivities : I have created a Middleware that will be used to authorize only the request coming from non frozen FoodBusinesses.

---
+++ src/Libraries/SmartRestaurant.Application/Common/Exceptions/FoodBusinessActivityFrozenException.cs
@@ -0,0 +1,10 @@
+namespace SmartRestaurant.Application.Common.Exceptions
+{
+    public class FoodBusinessActivityFrozenException : BaseException
+    {
+        public FoodBusinessActivityFrozenException(string foodBusinessName) : base(423, "FoodBusiness Activity Frozen")
+        {
+            Errors.Add($"The activity named '{foodBusinessName}' has been Frozen, please contact application administrator for more informations");
+        }
+    }  
+}
