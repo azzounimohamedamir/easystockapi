0f1cbf98b98eaec4d5081e0ef0a22e8413ea6105
Aissa Mahmoud bacha <ing_aissa@outlook.com>
2022-01-20 00:49:17 +0100

    [820] SR-UserFreezeDefreezeFoodBusinessActivityManually : fix merge conflict.

---
+++ src/Libraries/SmartRestaurant.Application/FoodBusiness/Commands/ToggleFoodBusinessFreezingStatusCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentValidation;
+using SmartRestaurant.Application.Common.Commands;
+using SmartRestaurant.Application.Common.Tools;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace SmartRestaurant.Application.FoodBusiness.Commands
+{
+    public class ToggleFoodBusinessFreezingStatusCommand : DeleteCommand
+    {
+        [SwaggerSchema(ReadOnly = true)] public string FoodBusinessId { get; set; }
+
+    }
+
+    public class ToggleFoodBusinessFreezingStatusCommandValidator : AbstractValidator<ToggleFoodBusinessFreezingStatusCommand>
+    {
+        public ToggleFoodBusinessFreezingStatusCommandValidator()
+        {
+            RuleFor(x => x.FoodBusinessId)
+                          .Cascade(CascadeMode.StopOnFirstFailure)
+                          .NotEmpty()
+                          .NotEqual(Guid.Empty.ToString())
+                          .Must(ValidatorHelper.ValidateGuid).WithMessage("'{PropertyName}' must be a valid GUID");
+        }
+    }
+}
