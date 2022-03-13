54483c3be3fe608b1ac951207a4a0b43c5d6e0db
soufianeBenRahim <32791470+soufianeBenRahim@users.noreply.github.com>
2022-03-13 12:51:18 +0100

    Add Query and query handler

---
+++ src/Libraries/SmartRestaurant.Application/Illness/Queries/GetDeshesIllnessQuery.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+using SmartRestaurant.Application.Common.Dtos;
+using SmartRestaurant.Application.Common.Dtos.DishDtos;
+using SmartRestaurant.Application.Common.Tools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartRestaurant.Application.Illness.Queries
+{
+    public class GetDeshesIllnessQuery : IRequest<IList<DisheIllnessDto>>
+    {
+        public List<string> disheIds { get; set; }
+        public List<string> illnesIds { get; set; }
+    }
+    public class GetDeshesIllnessQueryValidator : AbstractValidator<GetDeshesIllnessQuery>
+    {
+        public GetDeshesIllnessQueryValidator()
+        {
+            RuleFor(x => x.disheIds)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty()
+                .WithMessage("'{PropertyName}' must be a valid GUID");
+            RuleFor(x => x.illnesIds)
+                 .Cascade(CascadeMode.StopOnFirstFailure)
+                 .NotEmpty()
+                 .WithMessage("'{PropertyName}' must be a valid GUID");
+        }
+    }
+}
