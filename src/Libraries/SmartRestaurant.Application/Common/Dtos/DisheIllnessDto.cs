ee5694486bebebabe2a85f711024f46ad8e9c100
soufianeBenRahim <32791470+soufianeBenRahim@users.noreply.github.com>
2022-03-13 12:50:05 +0100

    Add Dto for corresponding Ingredients illness

---
+++ src/Libraries/SmartRestaurant.Application/Common/Dtos/DisheIllnessDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartRestaurant.Application.Common.Dtos
+{
+    public class DisheIllnessDto
+    {
+        public string IdDishe { get; set; }
+        public List<IllnessIngredientCorrespondingDto> IllnessIngredients { get; set; }
+    }
+}
