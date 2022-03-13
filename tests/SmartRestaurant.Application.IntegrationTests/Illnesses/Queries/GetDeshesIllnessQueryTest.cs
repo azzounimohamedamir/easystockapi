f6c5296cdab0b17d2dd0e664b952574a1fa304a0
soufianeBenRahim <32791470+soufianeBenRahim@users.noreply.github.com>
2022-03-13 12:52:26 +0100

    Add unit test of validator and integration test of Query handler

---
+++ tests/SmartRestaurant.Application.IntegrationTests/Illnesses/Queries/GetDeshesIllnessQueryTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SmartRestaurant.Application.Illness.Queries;
+using SmartRestaurant.Application.IntegrationTests.TestTools;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SmartRestaurant.Application.IntegrationTests.Illnesses.Queries
+{
+    using static Testing;
+    [TestFixture]
+    public class GetDeshesIllnessQueryTest : TestBase
+    {
+        [Test]
+        public async Task GetDeshesIllnessQuery_whenDesheContainIngredientAndExisteIllnessWitheThisIngredient_shouldRetunTheDsheIdWithEasheIngredienWithTheCorespondingIllnes()
+        {
+            await RolesTestTools.CreateRoles();
+            var foodBusinessAdministrator = await UsersTestTools.CreateFoodBusinessAdministrator(_authenticatedUserId);
+            var fastFood = await FoodBusinessTestTools.CreateFoodBusiness(foodBusinessAdministrator.Id);
+            var createIngredientCommand = await IngredientTestTools.CreateIngredient();
+            var createIngredientCommand2 = await IngredientTestTools.CreateIngredient();
+            var createDishCommand = await DishTestTools.CreateDish(fastFood.FoodBusinessId, createIngredientCommand.Id);
+            var createIllnessCommand = await IllnessTestTools.CreateIllnesswithDeffirentIngredients(createIngredientCommand.Id, createIngredientCommand2.Id);
+
+            var listDishesIllness = await SendAsync(new GetDeshesIllnessQuery { 
+                disheIds=new List<string>() { createDishCommand.Id.ToString() } ,
+                illnesIds = new List<string>() { createIllnessCommand.Id.ToString() },
+            });
+            listDishesIllness.Should().NotBeNull();
+
+            listDishesIllness[0].IdDishe.Should().Be(createDishCommand.Id.ToString());
+            listDishesIllness[0].IllnessIngredients.Should().HaveCount(1);
+            listDishesIllness[0].IllnessIngredients[0].IngredientId.Should().Be(createIngredientCommand.Id.ToString());
+            listDishesIllness[0].IllnessIngredients[0].Illness.Should().HaveCount(1);
+            listDishesIllness[0].IllnessIngredients[0].Illness[0].IllnessId.Should().Be(createIllnessCommand.Id.ToString());
+            listDishesIllness[0].IllnessIngredients[0].Illness[0].Name.Should().Be(createIllnessCommand.Name);
+        }
+    }
+}
---
+++ tests/SmartRestaurant.Application.Tests/CommandValidatorTests/Illnesses/Queries/GetDeshesIllnessQueryValidatorTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation.TestHelper;
+using SmartRestaurant.Application.Illness.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SmartRestaurant.Application.Tests.CommandValidatorTests.Illnesses.Queries
+{
+    public class GetDeshesIllnessQueryValidatorTest
+    {
+        private readonly GetDeshesIllnessQueryValidator _validator;
+
+        public GetDeshesIllnessQueryValidatorTest()
+        {
+            _validator = new GetDeshesIllnessQueryValidator();
+        }
+
+        [Fact]
+        public void Given_ListOfDishesIsEmpty_WhenValidating_ShouldGetAnError()
+        {
+            var empty = new List<string>();
+            _validator.ShouldHaveValidationErrorFor(x => x.disheIds, empty);
+        }
+        [Fact]
+        public void Given_ListOfIllnessIsEmpty_WhenValidating_ShouldGetAnError()
+        {
+            var empty = new List<string>();
+            _validator.ShouldHaveValidationErrorFor(x => x.illnesIds, empty);
+        }
+        [Fact]
+        public void Given_ListOfDishesIsNull_WhenValidating_ShouldGetAnError()
+        {
+            List<string> _null = null;
+            _validator.ShouldHaveValidationErrorFor(x => x.disheIds, _null);
+        }
+        [Fact]
+        public void Given_ListOfIllnessIsNull_WhenValidating_ShouldGetAnError()
+        {
+            List<string> _null = null;
+            _validator.ShouldHaveValidationErrorFor(x => x.illnesIds, _null);
+        }
+    }
+}
