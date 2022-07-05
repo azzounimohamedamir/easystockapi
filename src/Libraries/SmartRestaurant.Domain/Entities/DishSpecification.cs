1a2377480c6c46bae9065a1fad0236b78b5a7562
Rahim soufiane <32791470+soufianeBenRahim@users.noreply.github.com>
2022-07-05 16:22:53 +0100

    add entities fro dish and order

--- src/Libraries/SmartRestaurant.Domain/Entities/DishSpecification.cs
+++ src/Libraries/SmartRestaurant.Domain/Entities/DishSpecification.cs
@@ -1,5 +1,7 @@
 ﻿using SmartRestaurant.Domain.Enums;
+using SmartRestaurant.Domain.ValueObjects;
 using System;
+using System.Collections.Generic;
 
 namespace SmartRestaurant.Domain.Entities
 {
@@ -7,9 +9,11 @@ namespace SmartRestaurant.Domain.Entities
     {
         public Guid DishSpecificationId { get; set; }
         public string Title { get; set; }
+        public Names Names { get; set; }
         public ContentType ContentType { get; set; }
         public bool CheckBoxContent { get; set; }
         public string ComboBoxContent { get; set; }
+        public List<DishComboBoxItemTranslation> ComboBoxContentTranslation { get; set; }
         public Guid DishId { get; set; }
     }
 }
---
+++ src/Libraries/SmartRestaurant.Domain/Entities/OrderComboBoxItemTranslation.cs
@@ -0,0 +1,13 @@
+﻿using SmartRestaurant.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartRestaurant.Domain.Entities
+{
+    public class OrderComboBoxItemTranslation :TranslationItem
+    {
+        public Guid OrderComboBoxItemTranslationId { get; set; }
+        public Guid OrderDishSpecificationId { get; set; }
+    }
+}
