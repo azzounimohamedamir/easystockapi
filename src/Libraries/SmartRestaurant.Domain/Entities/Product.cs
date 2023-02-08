543132e6f683b0f0ec3558e7440a300b0bfd075b
ali-djemmal <alimenaouerdjemmal@gmail.com>
2023-02-08 08:16:44 +0100

    add new attribut to models

--- src/Libraries/SmartRestaurant.Domain/Entities/Product.cs
+++ src/Libraries/SmartRestaurant.Domain/Entities/Product.cs
@@ -7,8 +7,11 @@ namespace SmartRestaurant.Domain.Entities
     public class Product : MenuItem
     {
         public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public bool IsQuantityChecked { get; set; } 
         public virtual IList<SectionProduct> Sections { get; set; }
         public virtual IList<SubSectionProduct> SubSections { get; set; }
+
     }
 
 
