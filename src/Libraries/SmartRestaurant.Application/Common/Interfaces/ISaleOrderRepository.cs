107b3f53ffbc45b17c83c57e7504b4a9f10aa683
Abdelfetah NAMANE <namane.abdelfetah@gmail.com>
2023-03-09 15:45:48 +0100

    odoo search

--- src/Libraries/SmartRestaurant.Application/Common/Interfaces/ISaleOrderRepository.cs
+++ src/Libraries/SmartRestaurant.Application/Common/Interfaces/ISaleOrderRepository.cs
@@ -9,6 +9,7 @@ namespace SmartRestaurant.Application.Common.Interfaces
         Task<bool> Authenticate(Odoo info);
         Task<long> CreateAsync(string model,Dictionary<string,object> data);
         Task<long> UpdateAsync(string model,long odooId,Dictionary<string,object> data);
+        Task<T> Search<T>(string model, string attribute, string value, int limit);
 
 }
 
