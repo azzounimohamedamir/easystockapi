e639a3b143bb0abf2f838ede8522d43ca12427b2
ali-djemmal <alimenaouerdjemmal@gmail.com>
2023-03-19 22:51:24 +0100

    add price attribute to hotel : migration+commands

---
+++ src/Libraries/SmartRestaurant.Infrastructure/Migrations/20230319203603_AddRoomPrice.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartRestaurant.Infrastructure.Migrations
+{
+    public partial class AddRoomPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Rooms",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Rooms");
+        }
+    }
+}
