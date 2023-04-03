24945f9f42a47bff0092b0b47f80910b1663207f
Abdelfetah NAMANE <namane.abdelfetah@gmail.com>
2023-04-03 11:07:00 +0100

    firebase mocked

---
+++ tests/SmartRestaurant.Application.IntegrationTests/FirebaseServiceMocked.cs
@@ -0,0 +1,44 @@
+﻿using SmartRestaurant.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmartRestaurant.Application.IntegrationTests
+{
+	public class FirebaseServiceMocked : IFirebaseRepository
+	{
+        public Task<T> AddAsync<T>(string path, T data, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(data);
+        }
+        public Task<T> AddCollectionAsync<T>(string path, T data, CancellationToken cancellationToken) 
+        {
+            return Task.FromResult(data);
+        }
+        public Task<T> UpdateAsync<T>(string path, T data, CancellationToken cancellationToken) 
+        {
+            return Task.FromResult(data);
+        }
+        public Task<T> GetAsync<T>(string path, CancellationToken cancellationToken)
+        {
+            List<int> someValue = new List<int> { 2 };
+
+            T convertedValue = (T)Convert.ChangeType(someValue, typeof(T));
+
+            return Task.FromResult(convertedValue);
+        }
+        public Task<T> GetByIdAsync<T>(string path, string id, T data, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(data);
+        }
+        public Dictionary<string, object> getOrderToDictionary<T>(T orderDto)
+        {
+            return new Dictionary<string, object>
+            {
+                { "name", orderDto}
+            };
+            
+        }
+    }
+}
