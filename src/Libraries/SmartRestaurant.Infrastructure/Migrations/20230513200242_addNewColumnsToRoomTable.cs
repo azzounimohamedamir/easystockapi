d6ee5576830a0cbd0d57d93093c561386859d34f
ali-djemmal <alimenaouerdjemmal@gmail.com>
2023-05-13 21:05:20 +0100

    room migration

---
+++ src/Libraries/SmartRestaurant.Infrastructure/Migrations/20230513200242_addNewColumnsToRoomTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartRestaurant.Infrastructure.Migrations
+{
+    public partial class addNewColumnsToRoomTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Cleaned",
+                table: "Rooms",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCheckout",
+                table: "Rooms",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "DurationOfCheckin",
+                table: "Rooms",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDayOfCheckin",
+                table: "Rooms",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cleaned",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "DateCheckout",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "DurationOfCheckin",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "StartDayOfCheckin",
+                table: "Rooms");
+        }
+    }
+}
